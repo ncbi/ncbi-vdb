19a22798d17b90700aec24b3404be1bca3bcbe08
Andrew Klymenko <klymenka@ncbi.nlm.nih.gov>
2014-10-17 15:21:41 -0400

    1349: allow to specify custome path to 3-rd party libraries

--- package.pm
+++ package.pm
@@ -4,7 +4,9 @@ sub VERSION      { '2.4.2-1' }
 sub PACKAGE_TYPE { 'L' }
 sub PACKAGE_NAME { 'NCBI-VDB' }
 sub PACKAGE_NAMW { 'VDB' }
-sub DEPENDS      { qw(hdf5 xml2 magic) }
+sub DEPENDS      { ( { name => 'hdf5' , },
+                     { name => 'magic', Include => '/usr/include'        , },
+                     { name => 'xml2' , Include => '/usr/include/libxml2', } ) }
 sub CONFIG_OUT   { 'build' }
 sub PKG { ( LNG   => 'C',
             OUT   => 'ncbi-outdir',
@@ -13,6 +15,7 @@ sub PKG { ( LNG   => 'C',
 sub REQ { ( { name    => 'ngs-sdk',
               namew   => 'NGS',
               option  => 'with-ngs-sdk-prefix',
+              origin  => 'I',
               type    => 'I',
               srcpath => '../ngs/ngs-sdk',
               pkgpath => '/usr/local/ngs/ngs-sdk',
@@ -21,5 +24,14 @@ sub REQ { ( { name    => 'ngs-sdk',
               include => 'ngs/itf/Refcount.h',
               lib => 'libngs-sdk.so',
               ilib => 'libngs-bind-c++.a',
+        },
+        {     name    => 'hdf5',
+              option  => 'with-hdf5-prefix',
+              origin  => 'E',
+              type    => 'LI',
+              pkgpath => '/usr',
+              usrpath => '$HOME',
+              include => 'hdf5.h',
+              lib => 'libhdf5.a',
         } ) }
 1
