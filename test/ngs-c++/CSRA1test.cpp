61d405204e33670ff3bd26e299d52d8b09d1de51
aboshkin <boshkina@ncbi.nlm.nih.gov>
2015-04-01 16:02:16 -0400

    troubleshooting TeamCity build

--- test/ngs-c++/CSRA1test.cpp
+++ test/ngs-c++/CSRA1test.cpp
@@ -886,6 +886,7 @@ const char UsageDefaultName[] = "test-ngs_csra1-c++";
 
 rc_t CC KMain ( int argc, char *argv [] )
 {
+    KConfigDisableUserSettings();
     rc_t rc=NgsCsra1CppTestSuite(argc, argv);
     return rc;
 }
