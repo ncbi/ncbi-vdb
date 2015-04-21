f91c282803765b90ef39a593cf2af26a28649d62
klymenko <klymenka@mail.nih.gov>
2015-04-21 16:37:18 -0400

    1481: release version = 2.4.6a

--- libs/klib/release-vers.h
+++ libs/klib/release-vers.h
@@ -27,10 +27,10 @@
 /* THIS FILE IS NOT GENERATED AUTOMATICALLY! */
 
 /* The Version of current SRA Toolkit Release */
-#define RELEASE_VERS 0x02040005
+#define RELEASE_VERS 0x02040006
 
 /* The Revision of Version of current SRA Toolkit Release */
-#define RELEASE_REVISION 5
+#define RELEASE_REVISION 0
 
 /* The Type of Version of current SRA Toolkit Release is one of:
  *  'a' - alpha
@@ -38,4 +38,4 @@
  *  'c' - release candidate
  *  'r' - final
  */
-#define RELEASE_TYPE 'r'
+#define RELEASE_TYPE 'a'
