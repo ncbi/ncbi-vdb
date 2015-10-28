3c269ea4f94b5b677eafab32d7158a886766efe2
klymenko <klymenka@mail.nih.gov>
2015-10-28 15:11:11 -0400

    VDB-1429: minor change

--- libs/blast/read-id-type.h
+++ libs/blast/read-id-type.h
@@ -1,5 +1,5 @@
 #if 1
-    #define READ_ID_TYPE 0 // eFixedReadN
+    #define READ_ID_TYPE 0 /* eFixedReadN *************************************/
 #else
-    #define READ_ID_TYPE 1 // eFactor10
+    #define READ_ID_TYPE 1 /* eFactor10 */
 #endif
