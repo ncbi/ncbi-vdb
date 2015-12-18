c900cb10a5b582cee48c6f88099021cf665e8958
klymenko <klymenka@mail.nih.gov>
2015-12-18 10:38:03 -0500

    VDB-2819: release = 2.5.7

--- libs/klib/release-vers.h
+++ libs/klib/release-vers.h
@@ -27,7 +27,7 @@
 /* THIS FILE IS NOT GENERATED AUTOMATICALLY! */
 
 /* The Version of current SRA Toolkit Release */
-#define RELEASE_VERS 0x02050006
+#define RELEASE_VERS 0x02050007
 
 /* The Revision of Version of current SRA Toolkit Release */
 #define RELEASE_REVISION 0
