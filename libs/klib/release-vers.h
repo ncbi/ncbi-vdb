6409c3bc241c442ad1e9c06c9cdccd3d385ecf94
klymenko <klymenka@mail.nih.gov>
2016-01-12 16:27:17 -0500

    VDB-2840: 2.6.0b release: fixed

--- libs/klib/release-vers.h
+++ libs/klib/release-vers.h
@@ -30,7 +30,8 @@
 #define RELEASE_VERS 0x02060000
 
 /* The Revision of Version of current SRA Toolkit Release */
-#define RELEASE_REVISION b
+#define RELEASE_REVISION 0
+
 
 /* The Type of Version of current SRA Toolkit Release is one of:
  *  'a' - alpha
@@ -38,4 +39,4 @@
  *  'c' - release candidate
  *  'r' - final
  */
-#define RELEASE_TYPE 'r'
+#define RELEASE_TYPE 'b'
