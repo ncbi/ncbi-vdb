f3e9a1d41de079760e9ee30cb7493a3b199c90be
klymenko <klymenka@mail.nih.gov>
2016-06-30 15:06:27 -0400

    VDB-3084: release = 2.7.0

--- libs/blast/blast-mgr.c
+++ libs/blast/blast-mgr.c
@@ -50,7 +50,7 @@
 #include <stdio.h> /* fprintf */
 #include <string.h> /* memset */
 
-#define TOOLKIT "sratoolkit2_6_4"
+#define TOOLKIT "sratoolkit2_7_0"
 
 /******************************************************************************/
 
