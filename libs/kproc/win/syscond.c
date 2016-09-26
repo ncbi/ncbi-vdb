5339e595f30bdc3d4094afdce7257a111b6570e4
Andrii Nikitiuk <andrii.nikitiuk@nih.gov>
2016-09-26 17:41:40 -0400

    VDB-3005 Fixed KCondition on Windows to return timeout errors properly

--- libs/kproc/win/syscond.c
+++ libs/kproc/win/syscond.c
@@ -151,46 +151,57 @@ int pthread_cond_waitImpl (pthread_cond_t *cv,
 {
     int last_waiter;
     DWORD dwMilliseconds = INFINITE;
+    DWORD ret;
 
-  // Avoid race conditions.
-  EnterCriticalSection (&cv->waiters_count_lock_);
-  cv->waiters_count_++;
-  LeaveCriticalSection (&cv->waiters_count_lock_);
+    // Avoid race conditions.
+    EnterCriticalSection (&cv->waiters_count_lock_);
+    cv->waiters_count_++;
+    LeaveCriticalSection (&cv->waiters_count_lock_);
 
-  // This call atomically releases the mutex and waits on the
-  // semaphore until <pthread_cond_signal> or <pthread_cond_broadcast>
-  // are called by another thread.
-  if (!infinite && abstime != NULL)
-  { dwMilliseconds = ( DWORD ) ( abstime->tv_sec * 1000 + abstime->tv_nsec / 1000000 ) ; }
-  SignalObjectAndWait (*external_mutex, cv->sema_, dwMilliseconds, FALSE);
+    // This call atomically releases the mutex and waits on the
+    // semaphore until <pthread_cond_signal> or <pthread_cond_broadcast>
+    // are called by another thread.
+    if (!infinite && abstime != NULL)
+    { dwMilliseconds = ( DWORD ) ( abstime->tv_sec * 1000 + abstime->tv_nsec / 1000000 ) ; }
+    ret = SignalObjectAndWait (*external_mutex, cv->sema_, dwMilliseconds, FALSE);
 
-  // Reacquire lock to avoid race conditions.
-  EnterCriticalSection (&cv->waiters_count_lock_);
+    // Reacquire lock to avoid race conditions.
+    EnterCriticalSection (&cv->waiters_count_lock_);
 
-  // We're no longer waiting...
-  cv->waiters_count_--;
+    // We're no longer waiting...
+    cv->waiters_count_--;
 
-  // Check to see if we're the last waiter after <pthread_cond_broadcast>.
-  last_waiter = cv->was_broadcast_ && cv->waiters_count_ == 0;
+    // Check to see if we're the last waiter after <pthread_cond_broadcast>.
+    last_waiter = ret == WAIT_OBJECT_0 && cv->was_broadcast_ && cv->waiters_count_ == 0;
 
-  LeaveCriticalSection (&cv->waiters_count_lock_);
+    LeaveCriticalSection (&cv->waiters_count_lock_);
 
-  // If we're the last waiter thread during this particular broadcast
-  // then let all the other threads proceed.
-  if (last_waiter)
-    // This call atomically signals the <waiters_done_> event and waits until
-    // it can acquire the <external_mutex>.
-    // This is required to ensure fairness.
-    SignalObjectAndWait (cv->waiters_done_, *external_mutex, INFINITE, FALSE);
-  else {
-    // Always regain the external mutex since that's the guarantee we
-    // give to our callers. 
-/*      fprintf(stderr, "%s: WaitForSingleObject...\n", __func__); */
-    WaitForSingleObject (*external_mutex, INFINITE);
-/*      fprintf(stderr, "... %s: WaitForSingleObject\n", __func__); */
-  }
+    // If we're the last waiter thread during this particular broadcast
+    // then let all the other threads proceed.
+    if (last_waiter)
+        // This call atomically signals the <waiters_done_> event and waits until
+        // it can acquire the <external_mutex>.
+        // This is required to ensure fairness.
+        SignalObjectAndWait (cv->waiters_done_, *external_mutex, INFINITE, FALSE);
+    else {
+        // Always regain the external mutex since that's the guarantee we
+        // give to our callers.
+        /*      fprintf(stderr, "%s: WaitForSingleObject...\n", __func__); */
+        WaitForSingleObject (*external_mutex, INFINITE);
+        /*      fprintf(stderr, "... %s: WaitForSingleObject\n", __func__); */
+    }
 
-  return 0;
+    switch ( ret )
+    {
+    case WAIT_OBJECT_0:
+        break;
+    case WAIT_TIMEOUT:
+        return ETIMEDOUT;
+    default:
+        return EINVAL;
+    }
+
+    return 0;
 }
 
 int pthread_cond_timedwait(pthread_cond_t *cv,
@@ -440,8 +451,8 @@ LIB_EXPORT rc_t CC KConditionTimedWait ( KCondition *self, struct KLock *lock, t
     {
     case 0:
         break;
-/*  case ETIMEDOUT:
-        return RC ( rcPS, rcCondition, rcWaiting, rcTimeout, rcExhausted ); */
+    case ETIMEDOUT:
+        return RC ( rcPS, rcCondition, rcWaiting, rcTimeout, rcExhausted );
     case EINTR:
         return RC ( rcPS, rcCondition, rcWaiting, rcThread, rcInterrupted );
     default:
