32396e442fd7e5dadec58387a56dd75a5995d02a
wraetz <raetzw@ncbi.nlm.nih.gov>
2016-10-27 14:58:27 -0400

    test restricted to linux, 2 cases: out of space upfront and out of space while writing

--- test/kfs/run-cachetee-out-of-space-test.sh
+++ test/kfs/run-cachetee-out-of-space-test.sh
@@ -26,6 +26,39 @@
 RUN_DIR="$1"
 BIN_DIR="$2"
 
+echo "Out of space test #1 ... there is no space to create the cache-tee-file from the beginning"
+#=====================================================================================================
+
+#try to get rid of fuse-mount ( in case the test was terminated, before this script was able
+#to unmount the fuse-mount-point...
+fusermount -u $RUN_DIR/mount_point
+
+rm -rf $RUN_DIR/mount_point $RUN_DIR/proxy_point
+
+mkdir $RUN_DIR/mount_point
+rc=$?; if [[ $rc -ne 0 ]]; then echo "error creating mount-point"; exit $rc; fi
+
+mkdir $RUN_DIR/proxy_point
+rc=$?; if [[ $rc -ne 0 ]]; then echo "error creating proxy-point"; exit $rc; fi
+
+TRUNCATE_LIMIT="10k"
+WRITE_LIMIT="0"
+$BIN_DIR/fuse-proxy $RUN_DIR/mount_point $RUN_DIR/proxy_point $TRUNCATE_LIMIT $WRITE_LIMIT
+rc=$?; if [[ $rc -ne 0 ]]; then echo "error starting fuse-proxy"; exit $rc; fi
+
+$BIN_DIR/test-cachetee-out-of-space
+rc_test=$?
+
+fusermount -u $RUN_DIR/mount_point
+rc=$?; if [[ $rc -ne 0 ]]; then echo "error stopping fuse-proxy"; exit $rc; fi
+
+rm -rf $RUN_DIR/mount_point $RUN_DIR/proxy_point
+
+if [[ $rc_test -ne 0 ]]; then echo "error in test-cachetee-out-of-space binary"; exit $rc_test; fi
+
+
+echo "Out of space test #2 ... there is no space while using the cache-tee-file"
+#=====================================================================================================
 #try to get rid of fuse-mount ( in case the test was terminated, before this script was able
 #to unmount the fuse-mount-point...
 fusermount -u $RUN_DIR/mount_point
