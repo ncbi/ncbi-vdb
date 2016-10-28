5398eba16ca6fd3a7ecbadb227184f4d3028da9d
klymenko <klymenka@mail.nih.gov>
2016-10-28 13:12:23 -0400

    VDB-3200: switched to using k-services during resolving

--- libs/vfs/resolver-priv.h
+++ libs/vfs/resolver-priv.h
@@ -215,7 +215,7 @@ rc_t VResolverAlgParseResolverCGIResponse_3_0(const char *start,
 rc_t VResolverAlgRemoteProtectedResolve( const struct VResolverAlg *self,
     const struct KNSManager *kns, VRemoteProtocols protocols,
     const struct String *acc, const struct VPath **path,
-    const struct VPath **mapping, bool legacy_wgs_refseq);
+    const struct VPath **mapping, bool legacy_wgs_refseq, const char * version);
 
 /** get projectId ( valid for protected user repository ) */
 rc_t VResolverGetProjectId ( const VResolver * self, uint32_t * projectId );
@@ -232,7 +232,7 @@ rc_t VResolverGetProjectId ( const VResolver * self, uint32_t * projectId );
 rc_t VResolverRemoteResolve ( const VResolver *self,
     VRemoteProtocols protocols, const struct String * accession,
     const struct VPath ** path, const struct VPath **mapping,
-    const struct KFile ** opt_file_rtn, bool refseq_ctx, bool is_oid );
+    const struct KFile ** opt_file_rtn, bool refseq_ctx, bool is_oid, const char * version );
 
 
 void KConfigReadRemoteProtocols ( struct KConfig const * self, VRemoteProtocols * remote_protos );
