61e2aa12cde9ed7a2dbce0894f8f91c696a3b42c
kdurbrow <durbrowk@ncbi.nlm.nih.gov>
2017-06-20 13:07:27 -0400

    refactored; added test for zero-length window

--- test/align-access/test.cpp
+++ test/align-access/test.cpp
@@ -44,18 +44,17 @@ using namespace std;
 
 TEST_SUITE(IndexTestSuite);
 
-#if 1
-class LoaderFixture1
+struct Base
 {
+    struct TestSpec {
+        char const *ref;
+        int start;
+        int length;
+        int expected;
+        int result;
+    };
     AlignAccess::Database const db;
 
-    static std::string BAM_FILE_NAME(void) {
-#if MAC
-        return std::string("/net/traces04/giab05/ftp/data/AshkenazimTrio/HG002_NA24385_son/NIST_HiSeq_HG002_Homogeneity-10953946/NHGRI_Illumina300X_AJtrio_novoalign_bams/HG002.GRCh38.300x.bam");
-#else
-        return std::string("/netmnt/traces04/giab05/ftp/data/AshkenazimTrio/HG002_NA24385_son/NIST_HiSeq_HG002_Homogeneity-10953946/NHGRI_Illumina300X_AJtrio_novoalign_bams/HG002.GRCh38.300x.bam");
-#endif
-    }
     int test1Range(char const *ref, int const start, int const length) const {
         int alignments = 0;
         AlignAccess::AlignmentEnumerator e = db.slice(ref, start - 1, (start - 1) + length);
@@ -64,22 +63,11 @@ class LoaderFixture1
         }
         return alignments;
     }
-public:
-    LoaderFixture1() : db(AlignAccess::Manager::make().open(BAM_FILE_NAME()))
-    {
-    }
-    void testIndex(void) const {
-        struct {
-            char const *ref;
-            int start;
-            int length;
-            int expected;
-            int result;
-        } test[] = {
-            { "chr1", 100000001, 1, 347, -1 },
-            { "chr1", 141484030, 11733, 0, -1 },
-        };
-        int const n = 2;
+
+    Base(std::string const &file)
+    : db(AlignAccess::Manager::make().open(file))
+    {}
+    void testIndex(int const n, TestSpec test[]) const {
         bool failed = false;
         for (int i = 0; i < n; ++i)
             test[i].result = test1Range(test[i].ref, test[i].start, test[i].length);
@@ -91,6 +79,28 @@ public:
         }
         if (failed)
             throw std::runtime_error("failed");
+    }
+};    
+
+class LoaderFixture1 : private Base
+{
+    static std::string BAM_FILE_NAME(void) {
+#if MAC
+        return std::string("/net/traces04/giab05/ftp/data/AshkenazimTrio/HG002_NA24385_son/NIST_HiSeq_HG002_Homogeneity-10953946/NHGRI_Illumina300X_AJtrio_novoalign_bams/HG002.GRCh38.300x.bam");
+#else
+        return std::string("/netmnt/traces04/giab05/ftp/data/AshkenazimTrio/HG002_NA24385_son/NIST_HiSeq_HG002_Homogeneity-10953946/NHGRI_Illumina300X_AJtrio_novoalign_bams/HG002.GRCh38.300x.bam");
+#endif
+    }
+public:
+    LoaderFixture1() : Base(BAM_FILE_NAME())
+    {
+    }
+    void testIndex(void) const {
+        Base::TestSpec test[] = {
+            { "chr1", 100000001, 1, 347, -1 },
+            { "chr1", 141484030, 11733, 0, -1 },
+        };
+        Base::testIndex(2, test);
     }
 };    
 
@@ -98,13 +108,9 @@ FIXTURE_TEST_CASE ( LoadIndex1, LoaderFixture1 )
 {
     testIndex();
 }
-#endif
 
-#if 1
-class LoaderFixture2
+class LoaderFixture2 : private Base
 {
-    AlignAccess::Database const db;
-
     static std::string BAM_FILE_NAME(void) {
 #if MAC
         return std::string("/net/snowman/vol/projects/toolkit_test_data/traces04//1000genomes3/ftp/data/NA19240/exome_alignment/NA19240.mapped.SOLID.bfast.YRI.exome.20111114.bam");
@@ -112,41 +118,16 @@ class LoaderFixture2
         return std::string("/net/snowman/vol/projects/toolkit_test_data/traces04//1000genomes3/ftp/data/NA19240/exome_alignment/NA19240.mapped.SOLID.bfast.YRI.exome.20111114.bam");
 #endif
     }
-    int test1Range(char const *ref, int const start, int const length) const {
-        int alignments = 0;
-        AlignAccess::AlignmentEnumerator e = db.slice(ref, start, start + length);
-        while (e.next()) {
-            ++alignments;
-        }
-        return alignments;
-    }
 public:
-    LoaderFixture2() : db(AlignAccess::Manager::make().open(BAM_FILE_NAME()))
+    LoaderFixture2() : Base(BAM_FILE_NAME())
     {
     }
     void testIndex(void) const {
-        struct {
-            char const *ref;
-            int start;
-            int length;
-            int expected;
-            int result;
-        } test[] = {
+        Base::TestSpec test[] = {
             { "1", 1100001, 100000, 10387, -1 },
             { "1", 1200001, 100000, 14957, -1 },
         };
-        int const n = 2;
-        bool failed = false;
-        for (int i = 0; i < n; ++i)
-            test[i].result = test1Range(test[i].ref, test[i].start, test[i].length);
-        for (int i = 0; i < n; ++i) {
-            std::cout << "slice " << test[i].ref << ':' << test[i].start << '-' << (test[i].start - 1) + test[i].length << " should have " << test[i].expected << " alignments, got " << test[i].result << std::endl;
-            if (test[i].result != test[i].expected) {
-                failed = true;
-            }
-        }
-        if (failed)
-            throw std::runtime_error("failed");
+        Base::testIndex(2, test);
     }
 };    
 
@@ -154,7 +135,32 @@ FIXTURE_TEST_CASE ( LoadIndex2, LoaderFixture2 )
 {
     testIndex();
 }
+
+class LoaderFixture3 : private Base
+{
+    static std::string BAM_FILE_NAME(void) {
+#if MAC
+        return std::string("/net/snowman/vol/projects/toolkit_test_data/traces04/1000genomes3/ftp/data/NA19240/exome_alignment/NA19240.mapped.SOLID.bfast.YRI.exome.20111114.bam");
+#else
+        return std::string("/netmnt/traces04/1kg_pilot_data/ftp/pilot_data/data/NA10851/alignment/NA10851.SLX.maq.SRP000031.2009_08.bam");
 #endif
+    }
+public:
+    LoaderFixture3() : Base(BAM_FILE_NAME())
+    {
+    }
+    void testIndex(void) const {
+        Base::TestSpec test[] = {
+            { "1", 24725087, 0, 33272242, -1 },
+        };
+        Base::testIndex(1, test);
+    }
+};    
+
+FIXTURE_TEST_CASE ( LoadIndex3, LoaderFixture3 ) 
+{
+    testIndex();
+}
 
 static void test1Range(char const *path, char const *ref, int const start, int const end, bool const verbose)
 {
