16d4e7030d83bd69fe4221fcbc8c908364810dc4
wraetz <raetzw@ncbi.nlm.nih.gov>
2018-01-09 13:12:25 -0500

    improved cache behavior regarding increasing memory usage

--- libs/kfs/lru_cache.c
+++ libs/kfs/lru_cache.c
@@ -29,18 +29,18 @@
 #include <klib/data-buffer.h>
 #include <klib/vector.h>
 
-typedef struct lru_entry
+typedef struct lru_page
 {
     DLNode node;    /* to make it a valid entry into a DLL */
     uint64_t pos;
     uint64_t block_nr;
     KDataBuffer data;
-} lru_entry;
+} lru_page;
 
 typedef struct lru_cache
 {
     DLList lru;
-    KVector * entries;
+    KVector * page_lookup;
     KLock * lock;
     const KFile * wrapped;
 #if _DEBUGGING
@@ -48,17 +48,17 @@ typedef struct lru_cache
     void * handler_data;
 #endif
     size_t page_size;
-    uint32_t page_count;
-    uint32_t num_entries;
+    uint32_t max_pages;
+    uint32_t num_pages;
 } lru_cache;
 
-static void CC release_lru_entry( DLNode * n, void * data )
+static void CC release_lru_page( DLNode * n, void * data )
 {
     if ( n != NULL )
     {
-        lru_entry * entry = ( lru_entry * )n;
-        KDataBufferWhack ( &entry -> data );
-        free( ( void * ) entry );
+        lru_page * page = ( lru_page * )n;
+        KDataBufferWhack ( &page-> data );
+        free( ( void * ) page );
     }
 }
 
@@ -67,9 +67,9 @@ void release_lru_cache ( lru_cache * self )
     if ( self != NULL )
     {
         KLockRelease ( self -> lock );
-        if ( self -> entries != NULL )
-            KVectorRelease ( self -> entries );
-        DLListWhack ( &self -> lru, release_lru_entry, NULL );
+        if ( self -> page_lookup != NULL )
+            KVectorRelease ( self -> page_lookup );
+        DLListWhack ( &self -> lru, release_lru_page, NULL );
         free( ( void * ) self );
     }
 }
@@ -77,7 +77,7 @@ void release_lru_cache ( lru_cache * self )
 rc_t make_lru_cache ( lru_cache ** cache,
                       const KFile * wrapped,
                       size_t page_size,
-                      uint32_t page_count )
+                      uint32_t max_pages )
 {
     rc_t rc;
     KVector * v;
@@ -87,7 +87,7 @@ rc_t make_lru_cache ( lru_cache ** cache,
     *cache = NULL;
     if ( wrapped == NULL )
         return RC ( rcFS, rcFile, rcConstructing, rcParam, rcNull );
-    if ( page_size == 0 || page_count == 0  )
+    if ( page_size == 0 || max_pages == 0  )
         return RC ( rcFS, rcFile, rcConstructing, rcParam, rcInvalid );
 
     rc = KVectorMake ( &v );
@@ -103,9 +103,9 @@ rc_t make_lru_cache ( lru_cache ** cache,
             {
                 /* the work of DLListInit() aka setting head+tail to NULL
                    is already done by calloc() */
-                p -> entries = v;
+                p -> page_lookup = v;
                 p -> page_size = page_size;
-                p -> page_count = page_count;
+                p -> max_pages = max_pages;
                 p -> wrapped = wrapped;
                 *cache = p;
             }
@@ -118,8 +118,11 @@ rc_t make_lru_cache ( lru_cache ** cache,
     return rc;
 }
 
-static bool read_from_data_buffer( KDataBuffer * data, int64_t offset,
-                             uint64_t pos, void * buffer, size_t bsize, size_t * num_read )
+static bool read_from_data_buffer( KDataBuffer * data,      /* the data-buffer to read from */
+                                   int64_t offset,          /* the offset into the data-buffer */
+                                   void * buffer,           /* the buffer to read into */
+                                   size_t bsize,            /* how many bytes to read */
+                                   size_t * num_read )      /* how many bytes have been read */
 {
     int64_t available = ( data -> elem_count - offset );
     bool res = ( available > 0 );
@@ -135,52 +138,98 @@ static bool read_from_data_buffer( KDataBuffer * data, int64_t offset,
     return res;
 }
 
-static bool read_from_entry( lru_entry * entry,
-                             uint64_t pos, void * buffer, size_t bsize, size_t * num_read )
+static bool read_from_page( lru_page * page,
+                             uint64_t pos,
+                             void * buffer,
+                             size_t bsize,
+                             size_t * num_read )
 {
-    int64_t offset = ( pos - entry -> pos );
-    return read_from_data_buffer( &entry -> data, offset, pos, buffer, bsize, num_read );
+    int64_t offset = ( pos - page -> pos );
+    return read_from_data_buffer( &page -> data, offset, buffer, bsize, num_read );
 }
 
-static rc_t push_to_lru_cache( lru_cache * self, lru_entry * entry )
+static lru_page * get_tail_page( lru_cache * self )
+{
+    lru_page * res = NULL;
+    DLNode * node = DLListPopTail ( & self -> lru );
+    if ( node != NULL )
+    {
+        rc_t rc;
+        
+        res = ( lru_page * )node;
+
+        if ( self -> num_pages > 0 )
+            self -> num_pages--;
+        
+        rc = KVectorUnset ( self -> page_lookup, res -> block_nr );
+        
+#if _DEBUGGING
+        if ( rc == 0 && self -> handler != NULL )
+            self -> handler( self -> handler_data, CE_DISCARD, res -> pos, self -> page_size, res -> block_nr );
+#endif
+    }
+    return res;
+}
+
+static rc_t get_a_page( lru_cache * self, lru_page ** page )
 {
     rc_t rc = 0;
-    while ( rc == 0 && ( self -> num_entries >= self -> page_count ) )
+    *page = NULL;
+
+    /* this should not happen, there should never be more than self->num_pages in the cache */
+    while ( self -> num_pages > self -> max_pages )
     {
-        DLNode * node = DLListPopTail ( & self -> lru );
-        if ( self -> num_entries > 0 )
-            self -> num_entries--;
-        if ( node != NULL )
+        lru_page * p = get_tail_page( self );
+        release_lru_page( ( DLNode * )p, NULL );
+    }
+    
+    if ( self -> num_pages == self -> max_pages )
+    {
+        *page = get_tail_page( self );
+        if ( *page == NULL )
+            rc = RC ( rcFS, rcFile, rcConstructing, rcItem, rcInvalid );
+    }
+    else
+    {
+        lru_page * p = calloc( 1, sizeof * p );
+        if ( p == NULL )
+            rc = RC ( rcFS, rcFile, rcConstructing, rcItem, rcNull );
+        else
         {
-            lru_entry * e = ( lru_entry * )node;
-            rc = KVectorUnset ( self -> entries, e -> block_nr );
-#if _DEBUGGING
-            if ( rc == 0 && self -> handler != NULL )
-                self -> handler( self -> handler_data, CE_DISCARD, e -> pos, self -> page_size, e -> block_nr );
-#endif
-            release_lru_entry( node, NULL );
+            rc = KDataBufferMakeBytes( &( p -> data ), self -> page_size );
+            if ( rc == 0 )
+                *page = p;
+            else
+                free( ( void * ) p );
         }
     }
+    return rc;
+}
+
+static rc_t push_to_lru_cache( lru_cache * self, lru_page * page )
+{
+    rc_t rc = KVectorSetPtr ( self -> page_lookup, page -> block_nr, page );
     if ( rc == 0 )
-        rc = KVectorSetPtr ( self -> entries, entry -> block_nr, entry );
+        DLListPushHead ( & self -> lru, ( DLNode * )page );
     if ( rc == 0 )
-        DLListPushHead ( & self -> lru, ( DLNode * )entry );
-    if ( rc == 0 )
-        self -> num_entries++;
+        self -> num_pages++;
 
 #if _DEBUGGING
     if ( rc == 0 && self -> handler != NULL )
-        self -> handler( self -> handler_data, CE_ENTER, entry -> pos, self -> page_size, entry -> block_nr );
+        self -> handler( self -> handler_data, CE_ENTER, page -> pos, self -> page_size, page -> block_nr );
 #endif
     return rc;
 }
 
 static rc_t new_entry_in_lru_cache ( lru_cache * self,
-                      uint64_t pos, void * buffer, size_t bsize, size_t * num_read )
+                                     uint64_t pos,
+                                     void * buffer,
+                                     size_t bsize,
+                                     size_t * num_read )
 {
     rc_t rc = 0;
     uint64_t first_block_nr = ( pos / self -> page_size );
-    uint64_t last_block_nr = ( ( pos + bsize ) / self -> page_size );
+    uint64_t last_block_nr = ( ( pos + bsize - 1 ) / self -> page_size );
     uint64_t block_count = ( ( last_block_nr - first_block_nr ) + 1 );
     uint64_t blocks = 1; /* we are not testing block #0, this has been tested by the
                            caller already */
@@ -190,7 +239,7 @@ static rc_t new_entry_in_lru_cache ( lru_cache * self,
     while ( !done && blocks < block_count )
     {
         void * ptr;
-        done = ( 0 == KVectorGetPtr ( self -> entries, first_block_nr + blocks, &ptr ) );
+        done = ( 0 == KVectorGetPtr ( self -> page_lookup, first_block_nr + blocks, &ptr ) );
         if ( !done )
             blocks++;
     }
@@ -213,32 +262,41 @@ static rc_t new_entry_in_lru_cache ( lru_cache * self,
             {
                 /* give the buffer to the caller */
                 int64_t offset = ( pos - first_pos );
-                if ( !read_from_data_buffer( &data, offset, pos, buffer, bsize, num_read ) )
+                if ( !read_from_data_buffer( &data, offset, buffer, bsize, num_read ) )
                     rc = RC ( rcFS, rcFile, rcConstructing, rcTransfer, rcInvalid );
                 else
                 {
                     /* now portion the data-buffer into blocks and insert them... */
                     uint64_t block = 0;
                     uint64_t data_offset = first_block_nr * self -> page_size;
+                    uint64_t available = data . elem_count;
                     while ( rc == 0 && block < blocks )
                     {
-                        lru_entry * entry = calloc( 1, sizeof * entry );
-                        if ( entry == NULL )
-                            rc = RC ( rcFS, rcFile, rcConstructing, rcMemory, rcExhausted );
-                        else
+                        lru_page * page;
+                        rc = get_a_page( self, &page );
+                        if ( rc == 0 )
                         {
-                            rc = KDataBufferSub ( &data, &entry -> data, block * self -> page_size, self -> page_size );
+                            uint8_t * src = data . base;
+                            
+                            src += ( block * self -> page_size );
+                            if ( available > self -> page_size )
+                                page -> data . elem_count = self -> page_size;
+                            else
+                                page -> data . elem_count = available;
+                            memmove( page -> data . base, src, page -> data . elem_count );
+                            page -> pos = data_offset;
+                            page -> block_nr = first_block_nr + block;
+                            rc = push_to_lru_cache( self, page );
+                            available -= page -> data . elem_count;
                             if ( rc == 0 )
                             {
-                                entry -> pos = data_offset;
-                                entry -> block_nr = first_block_nr + block;
-                                rc = push_to_lru_cache( self, entry );
+                                block++;
+                                data_offset += self -> page_size;
                             }
-                            if ( rc != 0 )
-                                release_lru_entry( ( DLNode * )entry, NULL );
                         }
-                        block++;
-                        data_offset += self -> page_size;
+                        
+                        if ( rc != 0 )
+                            release_lru_page( ( DLNode * )page, NULL );
                     }
                 }
             }
@@ -249,31 +307,26 @@ static rc_t new_entry_in_lru_cache ( lru_cache * self,
     else
     {
         /* we have to produce just one block */
-        lru_entry * entry = calloc( 1, sizeof * entry );
-        if ( entry == NULL )
-            rc = RC ( rcFS, rcFile, rcConstructing, rcMemory, rcExhausted );
-        else
+        lru_page * page;
+        rc = get_a_page( self, &page );
+        if ( rc == 0 )
         {
-            rc = KDataBufferMakeBytes( & entry -> data, self -> page_size );
+            page -> pos = first_block_nr * self -> page_size;
+            page -> block_nr = first_block_nr;
+            rc = KFileReadAll ( self -> wrapped,
+                                page -> pos,
+                                page -> data . base,
+                                self -> page_size,
+                                &page -> data . elem_count );
             if ( rc == 0 )
             {
-                entry -> pos = first_block_nr * self -> page_size;
-                entry -> block_nr = first_block_nr;
-                rc = KFileReadAll ( self -> wrapped,
-                                    entry -> pos,
-                                    entry -> data . base,
-                                    self -> page_size,
-                                    &entry -> data . elem_count );
-                if ( rc == 0 )
-                {
-                    if ( read_from_entry( entry, pos, buffer, bsize, num_read ) )
-                        rc = push_to_lru_cache( self, entry );
-                    else
-                        rc = RC ( rcFS, rcFile, rcConstructing, rcTransfer, rcInvalid );
-                }
+                if ( read_from_page( page, pos, buffer, bsize, num_read ) )
+                    rc = push_to_lru_cache( self, page );
+                else
+                    rc = RC ( rcFS, rcFile, rcConstructing, rcTransfer, rcInvalid );
             }
             if ( rc != 0 )
-                release_lru_entry( ( DLNode * )entry, NULL );
+                release_lru_page( ( DLNode * )page, NULL );
         }
     }
     return rc;
@@ -282,7 +335,10 @@ static rc_t new_entry_in_lru_cache ( lru_cache * self,
 enum lookupres { DONE, RD_WRAPPED, NOT_FOUND };
 
 rc_t read_lru_cache ( lru_cache * self,
-                      uint64_t pos, void * buffer, size_t bsize, size_t * num_read )
+                      uint64_t pos,
+                      void * buffer,
+                      size_t bsize,
+                      size_t * num_read )
 {
     rc_t rc = 0;
     enum lookupres lr = NOT_FOUND;
@@ -303,23 +359,23 @@ rc_t read_lru_cache ( lru_cache * self,
             self -> handler ( self -> handler_data, CE_REQUEST, pos, bsize, block_nr );
 #endif
 
-        rc = KVectorGetPtr ( self -> entries, block_nr, &ptr );
+        rc = KVectorGetPtr ( self -> page_lookup, block_nr, &ptr );
         if ( rc == 0 )
         {
             /* we found the block in the entries! */
             lr = RD_WRAPPED;
             if ( ptr != NULL )
             {
-                lru_entry * entry = ptr;
-                if ( read_from_entry( entry, pos, buffer, bsize, num_read ) )
+                lru_page * page = ptr;
+                if ( read_from_page( page, pos, buffer, bsize, num_read ) )
                 {
                     /* put this entry at the top of the LRU-list */
-                    DLListUnlink ( & self -> lru, ( DLNode * )entry );
-                    DLListPushHead ( & self -> lru, ( DLNode * )entry );
-                    lr = DONE;
+                    DLListUnlink ( & self -> lru, ( DLNode * )page );
+                    DLListPushHead ( & self -> lru, ( DLNode * )page );
+                    lr = DONE;  /* we are done! */
 #if _DEBUGGING
                     if ( self -> handler != NULL )
-                        self -> handler ( self -> handler_data, CE_FOUND, pos, self -> page_size, entry -> block_nr );
+                        self -> handler ( self -> handler_data, CE_FOUND, pos, self -> page_size, page -> block_nr );
 #endif
                 }
             }
