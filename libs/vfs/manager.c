7162164cba5a16b94d56baddfc734c6abab40296
kwrodarmer <rodarmer@ncbi.nlm.nih.gov>
2018-07-23 14:57:01 -0400

    updated text to conform to English

--- libs/vfs/manager.c
+++ libs/vfs/manager.c
@@ -644,7 +644,7 @@ rc_t GetEncryptionKey(const VFSManager * self, const VPath * vpath, char* obuff,
     if ( GetRCState ( rc ) == rcNoPerm && GetRCObject ( rc ) == ( enum RCObject ) rcEncryptionKey )
     {
         LOGMSG ( klogErr, "You do not have read permissions to decrypt data from this project." );
-        LOGMSG ( klogErr, "Please contact your PI to request a NGC token with decrypt permissions." );
+        LOGMSG ( klogErr, "Please contact your PI to request an NGC token with decrypt permissions." );
         LOGMSG ( klogErr, "Import the new NGC file before decrypting again." );
         LOGMSG ( klogErr, "If you continue to have problems, contact sra@ncbi.nlm.nih.gov for assistance." );
     }
