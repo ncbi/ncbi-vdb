4d8a7a0478293465d06b9b4a77aa6dd9f4ce9d5d
ksrodarmer <rodarme1@ncbi.nlm.nih.gov>
2018-11-26 13:02:07 -0500

    ported c++ base64 to c in klib

---
+++ interfaces/klib/base64.h
@@ -0,0 +1,56 @@
+/*==============================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ */
+
+#ifndef _h_klib_base64_
+#define _h_klib_base64_
+
+#ifndef _h_klib_extern_
+#include <klib/extern.h>
+#endif
+
+#ifndef _h_klib_defs_
+#include <klib/defs.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct String;
+struct KDataBuffer;
+
+rc_t encodeBase64 ( struct String const ** encoded, const void * data, size_t bytes );
+rc_t decodeBase64 ( struct KDataBuffer * decoded, struct String const * encoding );
+
+rc_t encodeBase64URL ( struct String const ** encoded, const void * data, size_t bytes );
+rc_t decodeBase64URL ( struct KDataBuffer * decoded, struct String const * encoding );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _h_klib_base64_ */
