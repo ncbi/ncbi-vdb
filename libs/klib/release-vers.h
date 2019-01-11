31c92987a7eb7f621f9d4779fe6ae68cff4ce417
Andrew Klymenko <klymenka@nih.gov>
2019-01-11 14:34:42 -0500

    VDB-3694: call names cgi from trace.ncbi.nlm.nih.gov

--- libs/klib/release-vers.h
+++ libs/klib/release-vers.h
@@ -28,7 +28,7 @@
 
 
 /* Version of current SRA Toolkit Release */
-#define RELEASE_VERS 0x02090004
+#define RELEASE_VERS 0x02090005
 
 
 /* Type of Version of current SRA Toolkit Release is one of:
