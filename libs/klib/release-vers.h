bb682cef7f2c694cc0f96854cae1e5bb8d520b83
Andrew Klymenko <klymenka@nih.gov>
2019-03-14 15:37:17 -0400

    VDB-3726: 2.9.6 release

--- libs/klib/release-vers.h
+++ libs/klib/release-vers.h
@@ -28,7 +28,7 @@
 
 
 /* Version of current SRA Toolkit Release */
-#define RELEASE_VERS 0x02090005
+#define RELEASE_VERS 0x02090006
 
 
 /* Type of Version of current SRA Toolkit Release is one of:
@@ -38,5 +38,7 @@
  *  'c' - release candidate
  *  'r' - final
  */
-#define RELEASE_TYPE 'd'
+#define RELEASE_TYPE 'r'
+
+/* Revision of Version of current SRA Toolkit Release */
 #define RELEASE_REVISION 0
