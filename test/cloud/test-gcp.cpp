24c8fd71284f2b2f410eaaf97c771fec00d85ca7
aboshkin <boshkina@ncbi.nlm.nih.gov>
2019-07-09 13:13:51 -0400

    VDB-3780 started a GCP test suite

---
+++ test/cloud/test-gcp.cpp
@@ -0,0 +1,86 @@
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+* Tests for libs/cloud/gcp.c
+*/
+
+#include <cloud/cloud-priv.h>
+#include <cloud/manager.h>
+
+#include <kapp/args.h> /* ArgsMakeAndHandle */
+#include <kfg/config.h> /* KConfigDisableUserSettings */
+
+#include <ktst/unit_test.hpp>
+
+using std::string;
+
+static rc_t argsHandler(int argc, char* argv[]);
+TEST_SUITE_WITH_ARGS_HANDLER(AwsTestSuite, argsHandler)
+
+TEST_CASE(WithinGCP) 
+{
+}
+
+//////////////////////////////////////////// Main
+
+static rc_t argsHandler(int argc, char * argv[]) {
+    Args * args = NULL;
+    rc_t rc = ArgsMakeAndHandle(&args, argc, argv, 0, NULL, 0);
+    ArgsWhack(args);
+    return rc;
+}
+
+extern "C"
+{
+
+ver_t CC KAppVersion ( void )
+{
+    return 0x1000000;
+}
+rc_t CC UsageSummary (const char * progname)
+{
+    return 0;
+}
+
+rc_t CC Usage ( const Args * args )
+{
+    return 0;
+}
+const char UsageDefaultName[] = "test-gcp";
+
+rc_t CC KMain ( int argc, char *argv [] )
+{
+    KConfigDisableUserSettings();
+
+    // this makes messages from the test code appear
+    // (same as running the executable with "-l=message")
+    //TestEnv::verbosity = LogLevel::e_message;
+
+#ifdef TO_SHOW_RESULTS
+    assert(!KDbgSetString("KNS"));
+#endif
+
+    return AwsTestSuite(argc, argv);
+}
+
+}
