f67db1cec06edfe219990f403d988d6cb0e9a815
Andrew Klymenko <klymenka@nih.gov>
2019-10-17 12:49:11 -0400

    VDB-3956: more tests

--- test/vfs/test-perm.cpp
+++ test/vfs/test-perm.cpp
@@ -20,7 +20,7 @@ static rc_t argsHandler(int argc, char* argv[]) {
 
 TEST_SUITE_WITH_ARGS_HANDLER(TestPermSuite, argsHandler)
 
-TEST_CASE(TestPerm) {
+TEST_CASE(TestService) {
     /* service is NULL */
     REQUIRE_RC_FAIL(JwtKartValidateFile(NULL));
     REQUIRE_RC_FAIL(KServiceSetNgcFile(NULL, NULL));
@@ -46,22 +46,27 @@ TEST_CASE(TestPerm) {
     KFile * f = NULL;
     REQUIRE_RC(KDirectoryCreateFile(dir, &f, false,
         0664, kcmOpen | kcmInit | kcmCreate, path));
-    REQUIRE_RC_FAIL(KServiceSetNgcFile(service, path));
 
     /* empty cart cannot be valid */
     REQUIRE_RC_FAIL(KServiceSetJwtKartFile(service, path));
 
+    REQUIRE_RC_FAIL(KServiceSetNgcFile(service, path));
+
     REQUIRE_RC(KFileWriteExactly(f, 0, "1", 1));
     REQUIRE_RC_FAIL(KServiceSetNgcFile(service, path));
 
+    REQUIRE_RC(KFileWriteExactly(f, 0, "1", 1));
     /* too small cart cannot be valid */
     REQUIRE_RC_FAIL(KServiceSetJwtKartFile(service, path));
 
-    REQUIRE_RC(KFileWriteExactly(f, 1, ".2.3", 4));
     REQUIRE_RC_FAIL(KServiceSetNgcFile(service, path));
+
+    REQUIRE_RC(KFileWriteExactly(f, 1, ".2.3", 4));
 
     /* minimum size (now 5) of valid cart file has to be defined */
     REQUIRE_RC(KServiceSetJwtKartFile(service, path));
+
+    REQUIRE_RC_FAIL(KServiceSetNgcFile(service, path));
 
     REQUIRE_RC(KDirectoryRemove(dir, false, path));
     REQUIRE_RC(KFileRelease(f));
@@ -70,8 +75,10 @@ TEST_CASE(TestPerm) {
     /* a valid ngc file */
     REQUIRE_RC(KServiceSetNgcFile(service, "prj_phs710EA_test.ngc"));
 
-    REQUIRE_RC_FAIL(JwtKartValidateString(NULL));
+    REQUIRE_RC(KServiceRelease(service));
+}
 
+TEST_CASE(TestJwtKartValidateString) {
     String s;
     memset(&s, 0, sizeof s);
     REQUIRE_RC_FAIL(JwtKartValidateString(&s));
@@ -98,18 +105,111 @@ TEST_CASE(TestPerm) {
 
     /* a real jwt cart */
     const char * b = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImtleS1pZC0xMjM0IiwidHlwIjoiS"
-"ldUIn0.eyJleHAiOjE1Njk5NjAzODIsImlhdCI6MTU2OTM1NTU4MiwiaXNzIjoiZGJnYXBfcnVuX3N"
-"lbGVjdG9yIiwibGlzdCI6W3siY2F0YWxvZyI6IlNSQSIsImlkcyI6WyJTUlIwMDAwMDIiXX1dLCJ2Z"
-"XJzIjoxfQ.G7dfags0L0P89mVFg2WTlHyxvv9C4F1wLc6tiyzCzkSLwjIoijXqnqFhUBX0SnKCYyk-"
-"kKSrkJSs8u0aquxn8N8W8eYJ8vZ0Q8qeNSuQjfOPII6qV3re2dDdkjecgWvyulERN8uRMUiwUYipvN"
-"HxoFKQLzYhJajGt6p9rf8lNOPo1_AAuyLQeqCoEUY8bRTDnlVu90lU965eg2QZcvduQrI49p2aZIsC"
-"SV5e3TJj7SmXMlvNOJMlWD76rYoitWU82uA_S6mXXkTTxNd2KfmeEsj7g1djc1M7yUkQRET46eIp1q"
-"bk6gOAiiF5AL8eC8-SOLGrxqmdF8AU52_L31_pwA";
+        "ldUIn0.eyJleHAiOjE1Njk5NjAzODIsImlhdCI6MTU2OTM1NTU4MiwiaXNzIjoiZGJnYXBfcnVuX3N"
+        "lbGVjdG9yIiwibGlzdCI6W3siY2F0YWxvZyI6IlNSQSIsImlkcyI6WyJTUlIwMDAwMDIiXX1dLCJ2Z"
+        "XJzIjoxfQ.G7dfags0L0P89mVFg2WTlHyxvv9C4F1wLc6tiyzCzkSLwjIoijXqnqFhUBX0SnKCYyk-"
+        "kKSrkJSs8u0aquxn8N8W8eYJ8vZ0Q8qeNSuQjfOPII6qV3re2dDdkjecgWvyulERN8uRMUiwUYipvN"
+        "HxoFKQLzYhJajGt6p9rf8lNOPo1_AAuyLQeqCoEUY8bRTDnlVu90lU965eg2QZcvduQrI49p2aZIsC"
+        "SV5e3TJj7SmXMlvNOJMlWD76rYoitWU82uA_S6mXXkTTxNd2KfmeEsj7g1djc1M7yUkQRET46eIp1q"
+        "bk6gOAiiF5AL8eC8-SOLGrxqmdF8AU52_L31_pwA";
     size = strlen(b);
     StringInit(&s, b, size, size);
     REQUIRE_RC(JwtKartValidateString(&s));
+}
 
-    REQUIRE_RC(KServiceRelease(service));
+TEST_CASE(TestJwtKartValidateStringWhite) {
+    char b1[] = "abcdefghijklmnopqrstuvwxyz."
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ."
+        "0123456789-_\n"
+        "\r\n"
+        "\n\r"
+        "\r";
+    size_t size = sizeof b1 - 1;
+    String s;
+    StringInit(&s, b1, size, size);
+    REQUIRE_RC(JwtKartValidateString(&s));
+
+    /* check valid characters */
+    for (int i = 0; i < 256; ++i) {
+        b1[9] = i;
+        if ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') ||
+            (i >= '0' && i <= '9') || i == '-' || i == '_')
+        {
+            REQUIRE_RC(JwtKartValidateString(&s));
+        }
+        else
+            REQUIRE_RC_FAIL(JwtKartValidateString(&s));
+    }
+
+    /* check second section */
+    const char * b = "abcdefghijklmnopqrstuvwxyz."
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ.0123456789-_\n\r\n\n\r\r";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC(JwtKartValidateString(&s));
+
+    b = "abcdefghijklmnopqrstuvwxyz."
+        "ABCDEFGHIJKL NOPQRSTUVWXYZ.0123456789-_\n\r\n\n\r\r";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC_FAIL(JwtKartValidateString(&s));
+
+    /* check third section */
+    b = "abcdefghijklmnopqrstuvwxyz."
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ.0123456789-_\n\r\n\n\r\r";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC(JwtKartValidateString(&s));
+
+    b = "abcdefghijklmnopqrstuvwxyz."
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ.01234 6789-_\n\r\n\n\r\r";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC_FAIL(JwtKartValidateString(&s));
+}
+
+TEST_CASE(TestJwtKartValidateStringWhiteSections) {
+    const char * b = "1234567810123.567820123456.8301234567840";
+    size_t size = strlen(b);
+    String s;
+    StringInit(&s, b, size, size);
+    REQUIRE_RC(JwtKartValidateString(&s));
+
+    /* 1 dot */
+    b = "12345678101234567820123456.8301234567840";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC_FAIL(JwtKartValidateString(&s));
+
+    /* 3 dots */
+    b = "1234567810123.567820123456.830123.567840";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC_FAIL(JwtKartValidateString(&s));
+
+    /* first section empty */
+    b = ".2345678101234567820123456.8301234567840";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC_FAIL(JwtKartValidateString(&s));
+
+    /* first section size is min */
+    b = "1.345678101234567820123456.8301234567840";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC(JwtKartValidateString(&s));
+
+    /* second section empty */
+    b = "1234567810123456782012345..8301234567840";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC_FAIL(JwtKartValidateString(&s));
+
+    /* second section size is min */
+    b = "123456781012345678201234.6.8301234567840";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC(JwtKartValidateString(&s));
+
+    /* third section empty */
+    b = "1234567810123.5678201234567830123456784.";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC_FAIL(JwtKartValidateString(&s));
+
+    /* third section size is min */
+    b = "1234567810123.567820123456783012345678.0";
+    StringInit(&s, b, size, size);
+    REQUIRE_RC(JwtKartValidateString(&s));
 }
 
 const char UsageDefaultName[] = "test";
