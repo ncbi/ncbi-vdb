22ae8b8be0450813d1ba7c0804330e60bfda1283
Andrew Klymenko <klymenka@nih.gov>
2019-12-03 18:52:50 -0500

    VDB-4042: added tests for KSrvResponseGetLocation()

--- test/vfs/test-services.cpp
+++ test/vfs/test-services.cpp
@@ -25,12 +25,25 @@
 * tests of names service
 */
 
+#include <klib/debug.h> /* KDbgSetString */
+
 #include <ktst/unit_test.hpp> /* KMain */
 
+#include <vfs/path.h> /* VPathRelease */
 #include <vfs/services.h> /* KServiceRelease */
+
+#include <climits> /* PATH_MAX */
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+using std::string;
 
 TEST_SUITE ( TestServices );
 
+#define ALL
+
+#ifdef ALL
 TEST_CASE ( TestKServiceAddId ) {
     KService * s = NULL;
 
@@ -49,11 +62,75 @@ TEST_CASE ( TestKServiceAddId ) {
 
     REQUIRE_RC ( KServiceRelease ( s ) );
 }
+#endif
+
+#ifdef ALL
+TEST_CASE(TestKSrvResponseGetLocation) {
+    KService * s = NULL;
+    REQUIRE_RC(KServiceMake(&s));
+    REQUIRE_RC(KServiceAddId(s, "SRR850901"));
+    REQUIRE_RC(KServiceSetFormat(s, "all"));
+    const KSrvResponse * r = NULL;
+    REQUIRE_RC(KServiceNamesQuery(s, 0, &r));
+    REQUIRE_RC_FAIL(KSrvResponseGetLocation(0, 0, 0, 0, 0, 0, 0));
+    REQUIRE_RC_FAIL(KSrvResponseGetLocation(r, 0, 0, 0, 0, 0, 0));
+    REQUIRE_RC_FAIL(KSrvResponseGetLocation(r, "SRR850901", 0, 0, 0, 0, 0));
+    REQUIRE_RC_FAIL(KSrvResponseGetLocation(r, 0, "SRR850901", 0, 0, 0, 0));
+    REQUIRE_RC(KSrvResponseGetLocation(r, "SRR850901", "SRR850901",
+        0, 0, 0, 0));
+
+    const VPath * local = NULL;
+    REQUIRE_RC(KSrvResponseGetLocation(r, "SRR850901", "SRR850901",
+        &local, 0, 0, 0));
+    REQUIRE_NULL(local);
+
+    rc_t rcLocal = 0;
+    REQUIRE_RC_FAIL(KSrvResponseGetLocation(r, "SRR850901", "SRR850901",
+        0, &rcLocal, 0, 0));
+    REQUIRE_RC(KSrvResponseGetLocation(r, "SRR850901", "SRR850901",
+        &local, &rcLocal, 0, 0));
+    REQUIRE_NULL(local);
+    REQUIRE_RC_FAIL(rcLocal);
+
+    REQUIRE_RC(KSrvResponseGetLocation(r, "SRR850901", "SRR850901.vdbcache",
+        &local, &rcLocal, 0, 0));
+    REQUIRE_RC(rcLocal);
+    char buffer[PATH_MAX] = "";
+    REQUIRE_RC(VPathReadPath(local, buffer, sizeof buffer, NULL));
+    REQUIRE_EQ(string(buffer),
+        string("/netmnt/traces04/sra11/SRR/000830/SRR850901.vdbcache"));
+    REQUIRE_RC(VPathRelease(local));
+
+    const VPath * cache = NULL;
+    rc_t rcCache = 0;
+
+    REQUIRE_RC_FAIL(KSrvResponseGetLocation(r, "SRR850901", "SRR850901.qq",
+        &local, &rcLocal, &cache, &rcCache));
+    REQUIRE_NULL(local);
+    REQUIRE_NULL(cache);
+    REQUIRE_RC(rcLocal);
+    REQUIRE_RC(rcCache);
+
+    REQUIRE_RC_FAIL(KSrvResponseGetLocation(r, "SRR000001", "SRR000001",
+        &local, &rcLocal, &cache, &rcCache));
+    REQUIRE_NULL(local);
+    REQUIRE_NULL(cache);
+    REQUIRE_RC(rcLocal);
+    REQUIRE_RC(rcCache);
+
+    REQUIRE_RC(KSrvResponseRelease(r));
+    REQUIRE_RC(KServiceRelease(s));
+}
+#endif
 
 extern "C" {
     ver_t CC KAppVersion ( void ) { return 0; }
 
     rc_t CC KMain ( int argc, char * argv [] ) {
+
+        if (
+0) assert(!KDbgSetString("VFS"));
+
         return TestServices ( argc, argv );
     }
 }
