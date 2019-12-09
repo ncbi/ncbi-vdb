5acde7b1768e5bfad572b54dcf0b028a711431e1
wraetz <raetzw@ncbi.nlm.nih.gov>
2019-12-09 13:22:12 -0500

    new test added

--- test/kfs/cachetee3test.cpp
+++ test/kfs/cachetee3test.cpp
@@ -53,6 +53,9 @@
 #include <kfs/file.h>
 #include <kfs/recorder.h>
 
+#include <vfs/manager.h>
+#include <vfs/path.h>
+
 using namespace std;
 
 #define DATAFILE "org.dat"
@@ -1262,6 +1265,79 @@ FIXTURE_TEST_CASE ( CacheTee3_request_record, CT3Fixture )
     KOutMsg ( "recording requests done\n" );
 }
 #endif
+
+TEST_CASE( concurrent_reads_from_different_files )
+{
+    KOutMsg ( "concurrent reads from different files\n" );
+
+    std::string urls[] = {
+        "https://ftp.ncbi.nlm.nih.gov/toolbox/gbench/samples/udc_seqgraphic_rmt_testing/remote_BAM_remap_UUD-324/human/grch38_wgsim_gb_accs.bam",
+        "https://ftp.ncbi.nlm.nih.gov/toolbox/gbench/samples/udc_seqgraphic_rmt_testing/remote_BAM_remap_UUD-324/human/grch38_wgsim_gb_accs.bam.bai",
+        "https://ftp.ncbi.nlm.nih.gov/toolbox/gbench/samples/udc_seqgraphic_rmt_testing/remote_BAM_remap_UUD-324/yeast/yeast_wgsim_ucsc.bam",
+        "https://ftp.ncbi.nlm.nih.gov/toolbox/gbench/samples/udc_seqgraphic_rmt_testing/remote_BAM_remap_UUD-324/yeast/yeast_wgsim_ucsc.bam.bai"
+    };
+    
+    const size_t URL_COUNT = sizeof( urls ) / sizeof( urls[ 0 ] );
+    uint64_t sizes[ URL_COUNT ];
+    uint32_t magics[ URL_COUNT ];
+    
+    VFSManager* mgr = 0;
+    REQUIRE_RC( VFSManagerMake( &mgr ) );
+    for ( size_t i = 0; i < URL_COUNT; ++i )
+    {
+        VPath* path = 0;
+        REQUIRE_RC( VFSManagerMakePath( mgr, &path, urls[ i ].c_str() ) );
+
+        const KFile* file = 0;
+        REQUIRE_RC( VFSManagerOpenFileRead( mgr, &file, path ) );
+        
+        REQUIRE_RC( KFileSize( file, &sizes[ i ] ) );
+        REQUIRE_RC( KFileReadExactly( file, 0, &magics[ i ], sizeof( magics[ i ] ) ) );
+        std::cout << urls[ i ] <<": "<< sizes[ i ] << " " << std::hex << magics[ i ] << std::dec << std::endl;
+        REQUIRE_RC( KFileRelease( file ) );
+        REQUIRE_RC( VPathRelease( path ) );
+    }
+
+    const size_t NUM_PASSES = 2;
+    const size_t NUM_THREADS = 8;
+    for ( size_t pass = 0; pass < NUM_PASSES; ++pass )
+    {
+        vector< thread > tt( NUM_THREADS );
+        for ( size_t i = 0; i < NUM_THREADS; ++i )
+        {
+            size_t url_index = ( i % URL_COUNT );
+            tt[i] = thread( [&]( const string& url, uint64_t exp_size, uint32_t exp_magic )
+                {
+                   VPath* path = 0;
+                   const KFile* file = 0;
+                   uint64_t size = 0;
+                   uint32_t magic = 0;
+                   char buffer[ 20 * 1024 ];
+
+                   REQUIRE_RC( VFSManagerMakePath( mgr, &path, url.c_str() ) );
+                   REQUIRE_RC( VFSManagerOpenFileRead( mgr, &file, path ) );
+
+                   REQUIRE_RC( KFileSize( file, &size ) );
+                   REQUIRE_RC( KFileReadExactly( file, 0, &buffer, sizeof( buffer ) ) );
+                   memcpy( &magic, buffer, sizeof( magic ) );
+
+                   REQUIRE( size == exp_size );
+                   REQUIRE( magic == exp_magic );
+                   
+                   REQUIRE_RC( KFileRelease( file ) );
+                   REQUIRE_RC( VPathRelease( path ) );
+                }, urls[ url_index ], sizes[ url_index ], magics[ url_index ] );
+        }
+        for ( size_t i = 0; i < NUM_THREADS; ++i )
+        {
+            tt[ i ].join();
+        }
+    }
+
+    REQUIRE_RC( VFSManagerRelease( mgr ) );
+ 
+    KOutMsg ( "concurrent reads from different files done\n" );
+}
 
 //////////////////////////////////////////// Main
 extern "C" {
