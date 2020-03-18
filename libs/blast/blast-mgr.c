9c68a5074a147d55ba76bbe2039a81f4408d0790
Andrew Klymenko <klymenka@nih.gov>
2020-03-18 14:13:22 -0400

    VDB-4155: release = 2.10.5

--- libs/blast/blast-mgr.c
+++ libs/blast/blast-mgr.c
@@ -50,7 +50,7 @@
 #include <stdio.h> /* fprintf */
 #include <string.h> /* memset */
 
-#define TOOLKIT "sratoolkit2_10_4"
+#define TOOLKIT "sratoolkit2_10_5"
 
 /******************************************************************************/
 
