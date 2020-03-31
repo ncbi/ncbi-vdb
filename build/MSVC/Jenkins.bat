20c3a25237184d35b5dc820a05cd12b13b21eb00
aboshkin <boshkina@ncbi.nlm.nih.gov>
2020-03-31 11:24:04 -0400

    VDB-4138 adding a build script for Jenkins on Windows

--- build/MSVC/Jenkins.bat
+++ build/MSVC/Jenkins.bat
@@ -1,1 +1,13 @@
-echo Success!
+mkdir devel
+cd devel
+git clone -b engineering https://github.com/ncbi/ngs.git
+git clone -b 4138 https://github.com/ncbi/ncbi-vdb.git
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+
+cd ngs\ngs-sdk\win
+call C:\apache-ant-1.9.14\bin\ant build
+cd ngs\ngs-java
+call C:\apache-ant-1.9.14\bin\ant build
+cd ..\..\..\ncbi-vdb\build\MSVC
+call C:\apache-ant-1.9.14\bin\ant runtests
