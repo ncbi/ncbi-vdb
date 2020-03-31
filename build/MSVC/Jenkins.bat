636534608f7bf2b8e0f895c87040bb0a6f08388d
aboshkin <boshkina@ncbi.nlm.nih.gov>
2020-03-31 11:45:01 -0400

    VDB-4138 debugging the Jenkins script

--- build/MSVC/Jenkins.bat
+++ build/MSVC/Jenkins.bat
@@ -1,9 +1,13 @@
+del /F /S /Q devel 1>nul
+rd /s /q devel
+if exist devel rd /s /q devel
+
 mkdir devel
 cd devel
 git clone -b engineering https://github.com/ncbi/ngs.git
-git clone -b 4138 https://github.com/ncbi/ncbi-vdb.git
+git clone -b VDB-4138    https://github.com/ncbi/ncbi-vdb.git
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
 
 cd ngs\ngs-sdk\win
 call C:\apache-ant-1.9.14\bin\ant build
