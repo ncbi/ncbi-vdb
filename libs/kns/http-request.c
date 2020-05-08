f89b860a82bebbab7a687c44c0fccdd508a145c1
Andrew Klymenko <klymenka@nih.gov>
2020-05-08 17:13:12 -0400

    VDB-4239: added new header to HTTP requests to communicate SRA version

--- libs/kns/http-request.c
+++ libs/kns/http-request.c
@@ -26,12 +26,13 @@
 
 #include <kns/extern.h>
 
-#include <klib/rc.h>
-#include <klib/data-buffer.h>
-#include <klib/printf.h>
-#include <klib/log.h>
 #include <klib/base64.h>
+#include <klib/data-buffer.h>
 #include <klib/debug.h>
+#include <klib/log.h>
+#include <klib/printf.h>
+#include <klib/rc.h>
+#include <klib/sra-release-version.h> /* SraReleaseVersion */
 
 #include <cloud/manager.h>
 
@@ -1227,10 +1228,15 @@ rc_t CC KClientHttpRequestFormatMsgInt( const KClientHttpRequest *self,
 {
     rc_t rc;
     rc_t r2 = 0;
-    bool have_user_agent = false;
+
     bool have_accept = false;
-    String user_agent_string;
+    bool have_user_agent = false;
+    bool have_sra_release = false;
+
     String accept_string;
+    String sra_release_string;
+    String user_agent_string;
+
     const KHttpHeader *node;
 
     if ( self == NULL ) {
@@ -1240,8 +1246,9 @@ rc_t CC KClientHttpRequestFormatMsgInt( const KClientHttpRequest *self,
          return RC ( rcNS, rcNoTarg, rcReading, rcParam, rcNull );
     }
 
-    CONST_STRING ( &user_agent_string, "User-Agent" );
-    CONST_STRING ( &accept_string, "Accept" );
+    CONST_STRING ( &accept_string     , "Accept" );        /*  6 */
+    CONST_STRING ( &sra_release_string, "X-SRA-Release" ); /* 13 */
+    CONST_STRING ( &user_agent_string , "User-Agent" );    /* 10 */
 
     assert(method);
     if (method[0] != 'P') {
@@ -1273,11 +1280,36 @@ rc_t CC KClientHttpRequestFormatMsgInt( const KClientHttpRequest *self,
             if (StringCaseCompare(&node->name, &accept_string) == 0)
                 have_accept = true;
         }
+        /* look for "X-SRA-Release" */
+        else if (!have_sra_release && node->name.len == 13) {
+            if (StringCaseCompare(&node->name, &sra_release_string) == 0)
+                have_sra_release = true;
+        }
 
         /* add header line */
         rc = KDataBufferPrintf ( buffer, "%S: %S\r\n"
                                 , & node -> name
                                 , & node -> value );
+    }
+
+    /* add an Accept header if we did not find one already in the header tree */
+    if (!have_accept) {
+        r2 = KDataBufferPrintf(buffer, "Accept: */*\r\n");
+        if (rc == 0 && r2 != 0)
+            rc = r2;
+    }
+
+    /* add a X-SRA-Release header if we did not find one
+       already in the header tree */
+    if (!have_sra_release) {
+        SraReleaseVersion version;
+        r2 = SraReleaseVersionGet(&version);
+        if (r2 == 0) {
+            r2 = KDataBufferPrintf(buffer, "X-SRA-Release: %V\r\n",
+                version.version);
+        }
+        if (rc == 0 && r2 != 0)
+            rc = r2;
     }
 
     /* add an User-Agent header from the kns-manager if we did not find one already in the header tree */
@@ -1293,12 +1325,6 @@ rc_t CC KClientHttpRequestFormatMsgInt( const KClientHttpRequest *self,
                 rc = r2;
             }
         }
-    }
-
-    if (!have_accept) {
-        r2 = KDataBufferPrintf(buffer, "Accept: */*\r\n");
-        if (rc == 0 && r2 != 0)
-            rc = r2;
     }
 
     /* add terminating empty header line */
