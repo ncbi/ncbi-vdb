8f99d9392f06c8c5c39f5b9bcbafe2294c87472e
kdurbrow <kmdurbrow@me.com>
2020-06-04 10:59:10 -0400

    put back KFile

--- libs/kfg/docker.c
+++ libs/kfg/docker.c
@@ -34,6 +34,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <kfs/file.h>
+#include <kfs/directory.h>
+
+#define CGROUP_FILE_PATH "/proc/self/cgroup"
+/* /proc/self/cgroup is lines like this:
+ * 14:name=systemd:/docker/<container hash id>
+ * 14:name=systemd:/docker/2b644fadb9fbf627caeede280d3d00e9c4cc59022ae4ee186a92c41e96eb8106
+ */
 #endif
 
 int KConfig_Get_GUID_Add_Container(  char *const value
@@ -41,8 +49,58 @@ int KConfig_Get_GUID_Add_Container(  char *const value
 {
     if (value_size >= 12) {
 #if CAN_HAVE_CONTAINER_ID
-        char line[1024];
-        FILE *fp = fopen("/proc/self/cgroup", "r");
+        char buffer[4096];
+#if 1
+        size_t inbuf = 0;
+        size_t nread = 0;
+        uint64_t pos = 0;
+        KFile const *fp = NULL;
+        KDirectory *ndir = NULL;
+        rc_t rc = 0;
+        bool good = false;
+
+        rc = KDirectoryNativeDir(&ndir);
+        assert(rc == 0);
+
+        rc = KDirectoryOpenFileRead(ndir, &fp, CGROUP_FILE_PATH);
+        KDirectoryRelease(ndir);
+        if (rc == 0) {
+            LogErr(klogDebug, rc, "can not open " CGROUP_FILE_PATH);
+            return -1;
+        }
+        while (good == false && (rc = KFileRead(fp, pos, buffer + inbuf, sizeof(buffer) - inbuf, &nread)) == 0 && nread > 0)
+        {
+            size_t i = 0, start = 0;
+
+            pos += nread;
+            inbuf += nread;
+            for (i = 0; i < inbuf; ) {
+                int const ch = buffer[i++];
+                if (ch == ':') {
+                    start = i;
+                    continue;
+                }
+                if (ch == '\n') {
+                    char const *id = &buffer[start];
+                    size_t len = (i - 1) - start;
+                    if (len >= 8 && strncmp(id, "/docker/", 8) == 0) {
+                        id += 8; len -= 8;
+                        pLogMsg(klogDebug, "container-id: $(id)", "id=%.*s", (int)(len), id);
+                        if (len >= 12) {
+                            memmove(value + (value_size - 12), id, 12);
+                            good = true;
+                        }
+                    }
+                    inbuf -= i;
+                    memmove(buffer, buffer + i, inbuf);
+                    i = 0;
+                }
+            }
+        }
+        KFileRelease(fp);
+        return good ? 0 : -1;
+#else
+        FILE *fp = fopen(CGROUP_FILE_PATH, "r");
         if (fp) {
             while (fgets(line, sizeof(line), fp) != NULL) {
                 int start = 0;
@@ -78,6 +136,7 @@ int KConfig_Get_GUID_Add_Container(  char *const value
             }
             fclose(fp);
         }
+#endif
     }
 #endif
     return -1;
