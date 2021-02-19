4157a5ce380fd13717741b8745949ce5b37c5211
Andrew Klymenko <klymenka@nih.gov>
2021-02-19 12:04:47 -0500

    VDB-4434: 2.10.10 release

--- interfaces/klib/vdb_release_version.h
+++ interfaces/klib/vdb_release_version.h
@@ -26,6 +26,6 @@
 * ==============================================================================
 */
 
-#define VDB_RELEASE_VERSION 0x020A0009
+#define VDB_RELEASE_VERSION 0x020A000A
 
 #endif /* _h_klib_vdb_release_version_ */
--- libs/vdb/libvdb.vers.h
+++ libs/vdb/libvdb.vers.h
@@ -24,4 +24,4 @@
 *
 */
 
-#define LIBVDB_VERS 0x02070029
+#define LIBVDB_VERS 0x0207002A
