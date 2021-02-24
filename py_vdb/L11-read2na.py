6ea1ea688e11c48df91649306ba242f581c15d61
wraetz <raetzw@nih.gov>
2021-02-24 15:29:44 -0500

    more columns in copy-example and a run-script

--- py_vdb/L11-read2na.py
+++ py_vdb/L11-read2na.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, argparse, shutil, vdb
+import os, sys, argparse, shutil, vdb
 
 schema_txt = '''
 version 1;
@@ -11,105 +11,116 @@ table SEQUENCE #1.0
 {
     column <ascii> zip_encoding READ;
     column <U8> zip_encoding QUALITY;
+    column <U32> zip_encoding READ_LEN;
+    column <U32> zip_encoding READ_START;
+    column <ascii> zip_encoding NAME;
 };
 
 ''' 
 
-rd_read_col_name = "(INSDC:dna:text)READ"
-rd_qual_col_name = "(INSDC:quality:phred)QUALITY"
+#a list of name-tuples: (rd-name, wr-name )
+column_names = [ ( "(INSDC:dna:text)READ", "READ" ),
+                 ( "(INSDC:quality:phred)QUALITY", "QUALITY" ),
+                 ( "READ_LEN", "READ_LEN" ),
+                 ( "READ_START", "READ_START" ),
+                 ( "NAME", "NAME" ) ]
 
-wr_read_col_name = "READ"
-wr_qual_col_name = "QUALITY"
+class progressbar :
+    def __init__( self, width : int, max_value : int ) :
+        self.width = width
+        self.max_value = max_value
+        self.cur_value = 0
+        self.dashes = 0
+        sys.stdout.write( "[{}]".format( " " * width ) ) 
+        sys.stdout.flush()
+        sys.stdout.write( "\b" * ( width + 1 ) )
 
-tmp_name = "tmp"
+    def update( self ) :
+        new_dashes = ( self.width * self.cur_value ) // self.max_value
+        if new_dashes > self.dashes :
+            sys.stdout.write( "-" * ( new_dashes - self.dashes ) )
+            sys.stdout.flush()
+            self.dashes = new_dashes
 
-#"(INSDC:2na:packed)READ"
-#"(INSDC:2na:bin)READ"
-#"(INSDC:quality:text:phred_33)QUALITY"
+    def inc( self, by : int ) :
+        if by < 0 : return
+        self.cur_value += by
+        if self.cur_value > self.max_value : self.cur_value = self.max_value
+        self.update()
 
-def force_remove( dir ) :
-    try :
-        #if dir[ -1 ] == os.sep: dir = dir[ :-1 ]
-        for file in os.listdir( dir ) :
-            if file == '.' or file == '..': continue
-            path = dir + os.sep + file
-            if os.path.isdir( path ):
-                force_remove( path )
-            else:
-                os.unlink( path )
-        os.rmdir( dir )
-    except :
-        pass
+    def finish( self ) :
+        self.cur_value = self.max_value
+        self.update()
+        print( "" )
+        print( "{} rows.".format( self.max_value ) )
 
-def copy_table( rd_tbl, wr_tbl, first, count ) :
+def copy_table( rd_tbl, wr_tbl, first : int, count : int, freq : int ) :
+    rd_col_names = list()
+    wr_col_names = list()
+    for rd_name,wr_name in column_names :
+        rd_col_names.append( rd_name )
+        wr_col_names.append( wr_name )
+       
     rd_cur = rd_tbl.CreateCursor()
-    rd_cols = rd_cur.OpenColumns( [ rd_read_col_name, rd_qual_col_name ] )
-    rd_read_col = rd_cols[ rd_read_col_name ]
-    rd_qual_col = rd_cols[ rd_qual_col_name ]
-    
-    first_row, row_count = rd_read_col.row_range()
-    
+    rd_cols = rd_cur.OpenColumns( rd_col_names )
+
+    first_row, row_count = rd_cols[ rd_col_names[ 1 ] ].row_range()
     if first != None : first_row = first
     if count != None : row_count = count
-
+    
     wr_cur = wr_tbl.CreateCursor( vdb.OpenMode.Write )
-    wr_cols = wr_cur.OpenColumns( [ wr_read_col_name, wr_qual_col_name ] )
-    wr_read_col = wr_cols[ wr_read_col_name ]
-    wr_qual_col = wr_cols[ wr_qual_col_name ]
+    wr_cols = wr_cur.OpenColumns( wr_col_names )
 
+    columns = list()
+    for i in vdb.xrange( len( rd_col_names ) ) :
+        rdc = rd_cols[ rd_col_names[ i ] ]
+        wrc = wr_cols[ wr_col_names[ i ] ]
+        columns.append( ( rdc, wrc ) )
+    
+    pb = progressbar( 80, row_count )
     for row in vdb.xrange( first_row, first_row + row_count ) :
-        if row % 1000 == 0 : print( "row #{}".format( row ) )
-        wr_cur.OpenRow()
-        wr_read_col.write( rd_read_col.Read( row ) )
-        wr_qual_col.write( rd_qual_col.Read( row ) )
-        wr_cur.CommitRow()
-        wr_cur.CloseRow()
+        if row % freq == 0 : pb.inc( freq )
+        wr_cur.OpenRow()                        #this is the meat of the copy-operation
+        for rdc, wrc in columns :               #
+            wrc.write( rdc.Read( row ) )        #
+        wr_cur.CommitRow()                      #
+        wr_cur.CloseRow()                       #
     wr_cur.Commit()
-     
+    pb.finish()
+
 if __name__ == '__main__' :
     parser = argparse.ArgumentParser()
-    parser.add_argument( 'accession', nargs='*' )    
+    parser.add_argument( 'accession' )
     parser.add_argument( '-X', '--first', metavar='row-id', help='first row-id', type=int, dest='first' )
     parser.add_argument( '-N', '--count', metavar='rows', help='how many reads', type=int, dest='count' )
     parser.add_argument( '-R', '--readlib', metavar='path', help='read library', type=str, dest='readlib' )    
     parser.add_argument( '-W', '--writelib', metavar='path', help='write library', type=str, dest='writelib' )
-    parser.add_argument( '-O', '--output', metavar='path', help='output', type=str, dest='output', default="out" )    
+    parser.add_argument( '-O', '--output', metavar='path', help='output', type=str, dest='output', default="tmp" )
+    parser.add_argument( '-F', '--freq', metavar='number', help='progress freq', type=int, dest='freq', default=2000 )
     args = parser.parse_args()
 
     try :
+        print( "making a copy of : {}".format( args.accession ) )
         #open 2 managers:
         rd_mgr = vdb.manager( vdb.OpenMode.Read,  args.readlib )
         wr_mgr = vdb.manager( vdb.OpenMode.Write, args.writelib )
-        nr = 1
 
-        for acc in args.accession :
-            rd_tbl = None
-            pt = rd_mgr.PathType( acc ) #detect path-type ( database or table or anything-else )
-            if pt == vdb.PathType.Database :
-                rd_tbl = rd_mgr.OpenDB( acc ).OpenTable( "SEQUENCE" ) #object is a database
-            elif pt == vdb.PathType.Table :
-                rd_tbl = rd_mgr.OpenTable( acc ) #object is a table
-            else :
-                print( "%s is not an SRA-object"%( acc ) )
+        rd_tbl = None
+        pt = rd_mgr.PathType( args.accession ) #detect path-type ( database or table or anything-else )
+        if pt == vdb.PathType.Database :
+            rd_tbl = rd_mgr.OpenDB( args.accession ).OpenTable( "SEQUENCE" ) #object is a database
+        elif pt == vdb.PathType.Table :
+            rd_tbl = rd_mgr.OpenTable( args.accession ) #object is a table
+        else :
+            print( "%s is not an SRA-object"%( args.accession ) )
 
-            if rd_tbl != None :
-                force_remove( tmp_name )
-                schema = wr_mgr.MakeSchema( schema_txt )
-                wr_tbl = wr_mgr.CreateTable( schema, "SEQUENCE", tmp_name )
-                
-                if wr_tbl != None :
-                    copy_table( rd_tbl, wr_tbl, args.first, args.count )
-
-                    output_name = "{}.{}".format( args.output, nr )
-                    nr += 1
-                    try :
-                        os.remove( output_name )
-                    except :
-                        pass
-                    os.system( "kar -c {} -d {}".format( output_name, tmp_name ) )
+        if rd_tbl != None :
+            schema = wr_mgr.MakeSchema( schema_txt )
+            wr_tbl = wr_mgr.CreateTable( schema, "SEQUENCE", args.output )
+            if wr_tbl != None :
+                copy_table( rd_tbl, wr_tbl, args.first, args.count, args.freq )
 
     except vdb.vdb_error as e :
         print( e )
     except KeyboardInterrupt :
         print( "^C" )
-    force_remove( tmp_name )
