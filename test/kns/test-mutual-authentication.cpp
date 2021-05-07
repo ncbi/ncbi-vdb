1895afe2da5e52a2a22bd90f0c90909ded3df70f
Andrew Klymenko <klymenka@nih.gov>
2021-05-07 12:27:31 -0400

    VDB-4465: TLS mutual authentication test: start using Args API

--- test/kns/test-mutual-authentication.cpp
+++ test/kns/test-mutual-authentication.cpp
@@ -38,7 +38,7 @@ const char UsageDefaultName[] = "test-mutual-authentication";
 rc_t CC UsageSummary(const char * progname) { return 0; }
 rc_t CC Usage(const struct Args * args) { return 0; }
 
-static bool OwnCertfromEnv(const char * location,
+static bool LoadOwnCert(const char * location,
     const char ** own_cert, const char ** pk_key)
 {
     void ** cert = (void**)own_cert;
@@ -87,28 +87,46 @@ static bool OwnCertfromEnv(const char * location,
     return rc == 0;
 }
 
+#define S_ALIAS  "n"
+#define S_OPTION "no-singleton"
+static
+const char * S_USAGE [] = { "not to use KNSManager singleton", NULL };
+
+static
+OptDef Options[] = {
+   /* name      alias    help_gen help max_count needs_value required*/
+    { S_OPTION, S_ALIAS, NULL, S_USAGE,    1,       false,   false  },
+};
+
 rc_t CC KMain(int argc, char *argv[]) {
-    if (argc < 2) {
-        cerr << "Usage:\n"
-            << argv[0] << " <DIR> <URL>, [<DIR> <URL> ...]\n";
-    }
+    bool singleton = true;
 
     Args * args = NULL;
-    rc_t rc = ArgsMakeAndHandle(&args, argc, argv, 0);
-
-    bool singleton = true;
-    int first = 1;
-    if (strcmp(argv[first], "no-singleton") == 0) {
-        singleton = false;
-        ++first;
-    }
+    rc_t rc = ArgsMakeAndHandle(
+        &args, argc, argv, 1, Options, sizeof Options / sizeof Options[0]);
+    if (rc != 0)
+        return rc;
+    uint32_t pcount = 0;
+    do {
+        rc = ArgsOptionCount(args, S_OPTION, &pcount);
+        if (rc != 0)
+            break;
+        if (pcount > 0)
+            singleton = false;
+    } while (false);
 
     KNSManager * mgr = NULL;
     if (rc == 0)
         rc = singleton ? KNSManagerMake(&mgr) : KNSManagerMakeLocal(&mgr, 0);
 
-    for (int i = first; rc == 0 && i < argc; ++i) {
-        if (i > first && !singleton) {
+    rc = ArgsParamCount(args, &pcount);
+    if (rc == 0 && pcount == 0) {
+        cerr << "Usage:\n"
+            << argv[0] << " <DIR> <URL>, [<DIR> <URL> ...]\n";
+        rc = 1;
+    }
+    for (uint32_t i = 0; rc == 0 && i < pcount; ++i) {
+        if (i > 1 && !singleton) {
             rc_t r2 = KNSManagerRelease(mgr);
             mgr = NULL;
             if (r2 != 0 && rc == 0)
@@ -116,32 +134,39 @@ rc_t CC KMain(int argc, char *argv[]) {
             if (rc == 0)
                 rc = KNSManagerMakeLocal(&mgr, 0);
         }
-        if (argv[i][0] != '-' || argv[i][0] != '0') {
+        const char * v = NULL;
+        rc = ArgsParamValue(args, i, (const void **)&v);
+        if (rc != 0)
+            break;
+        assert(v);
+        if (v[0] != 'X' || v[0] != '0') {
             const char *own_cert = NULL, *pk_key = NULL;
-            if (OwnCertfromEnv(argv[i], &own_cert, &pk_key)) {
+            if (LoadOwnCert(v, &own_cert, &pk_key)) {
                 rc = KNSManagerSetOwnCert(mgr, own_cert, pk_key);
                 // second call to KNSManagerSetOwnCert should fail
-                if (i > 2 && singleton) {
+                if (i > 0 && singleton) {
                     if (rc == 0)
-                        rc = 1;
+                        rc = 2;
                     else
                         rc = 0;
                 }
                 if (rc == 0) {
                     rc = KNSManagerSetOwnCert(mgr, own_cert, pk_key);
                     if (rc == 0)
-                        rc = 1;
+                        rc = 3;
                     else
                         rc = 0;
                 }
             }
             free(const_cast<void*>(static_cast<const void*>(own_cert)));
             free(const_cast<void*>(static_cast<const void*>(pk_key)));
+        }
 
-            if (rc == 0 && ++i < argc) {
+        if (rc == 0 && ++i < pcount) {
+            rc = ArgsParamValue(args, i, (const void **)&v);
+            if (rc == 0) {
                 const KFile * file = NULL;
-                rc = KNSManagerMakeHttpFile(
-                    mgr, &file, NULL, 0x01010000, argv[i]);
+                rc = KNSManagerMakeHttpFile(mgr, &file, NULL, 0x01010000, v);
                 rc_t r2 = KFileRelease(file);
                 if (r2 != 0 && rc == 0)
                     rc = r2;
