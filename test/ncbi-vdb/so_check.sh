e52c1ed83bab0535cfffc9d82d7a577b72f2ab3c
Alexander Ukrainchik <ukrainch@ncbi.nlm.nih.gov>
2021-07-14 16:10:49 -0400

    VDB3-5 converted scripted tests (schema) to cmake

--- test/ncbi-vdb/so_check.sh
+++ test/ncbi-vdb/so_check.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 K=$(nm $1 | grep " U " | grep -v GLIBC | grep -v " U xml");
 if [ "$K" != "" ];
-	then ( echo "Error: $1 contains unresolved external references:\n $K" && exit 1 );
+	then echo "Error: $1 contains unresolved external references:\n $K" && exit 1;
 fi
