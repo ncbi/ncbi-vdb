61a272bcd4f692fd48bb57e79d65ebab4d43e364
Alexander Ukrainchik <ukrainch@ncbi.nlm.nih.gov>
2021-07-16 13:48:35 -0400

    VDB3-5 fixed kns scripted tests

---
+++ test/kns/test-proxy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+test_proxy=$1
+
+echo test-proxy BEGIN ...
+
+# no proxy specified: fail
+unset http_proxy
+output=$($test_proxy)
+res=$?
+if [ "$res" != "0" ];
+	then echo "test-proxy failed, res=$res output=$output"
+fi
+
+echo ... test-proxy: END
+
+exit 0
