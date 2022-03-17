3a8cde6e7ae4c066132d514105d1337d363f38c6
Alexander Ukrainchik <ukrainch@ncbi.nlm.nih.gov>
2022-03-17 11:50:11 -0400

    VDB-4830 adding asan and tsan at the build system top level

--- build/env.cmake
+++ build/env.cmake
@@ -32,6 +32,8 @@ if ( ${CMAKE_VERSION} VERSION_EQUAL "3.20" OR
     cmake_policy(SET CMP0115 OLD)
 endif()
 
+option( RUN_SANITIZER_TESTS "Run ASAN and TSAN tests" OFF )
+
 # ===========================================================================
 # set up CMake variables describing the environment
 
@@ -283,3 +285,29 @@ if ( SINGLE_CONFIG )
         "execute_process(COMMAND /bin/bash -c \"${CMAKE_SOURCE_DIR}/build/install-root.sh ${VERSION} ${INST_INCDIR} ${INST_LIBDIR} \" )"
     )
 endif()
+
+if( NOT SINGLE_CONFIG )
+	if( RUN_SANITIZER_TESTS )
+		message( "RUN_SANITIZER_TESTS (${RUN_SANITIZER_TESTS}) cannot be turned on in a non single config mode - overriding to OFF" )
+	endif()
+	set( RUN_SANITIZER_TESTS OFF )
+endif()
+
+if( RUN_SANITIZER_TESTS )
+	find_program(LSB_RELEASE_EXEC lsb_release)
+	execute_process(COMMAND ${LSB_RELEASE_EXEC} -is
+		OUTPUT_VARIABLE LSB_RELEASE_ID_SHORT
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	message("LSB_RELEASE_ID_SHORT: ${LSB_RELEASE_ID_SHORT}")
+	if( LSB_RELEASE_ID_SHORT STREQUAL "Ubuntu" )
+		message("Disabling sanitizer tests on Ubuntu...")
+		set( RUN_SANITIZER_TESTS OFF )
+	endif()
+endif()
+
+if( RUN_SANITIZER_TESTS_OVERRIDE )
+	message("Overriding sanitizer tests due to RUN_SANITIZER_TESTS_OVERRIDE: ${RUN_SANITIZER_TESTS_OVERRIDE}")
+	set( RUN_SANITIZER_TESTS ON )
+endif()
+message( "RUN_SANITIZER_TESTS: ${RUN_SANITIZER_TESTS}" )
