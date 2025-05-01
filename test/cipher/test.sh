214e20c24943c0ca0092647e9f69c9344181d76a
Andrew Klymenko <klymenka@ncbi.nlm.nih.gov>
2025-05-01 16:05:06 -0400

    check for failures

--- test/cipher/test.sh
+++ test/cipher/test.sh
@@ -15,16 +15,16 @@ fi
 tmp_py_env=$(pwd)/temp_env
 
 ${PYTHON} -V
-${VIRTUALENV} -p ${PYTHON} $tmp_py_env
-. $tmp_py_env/bin/activate
+${VIRTUALENV} -p ${PYTHON} $tmp_py_env || exit 1
+. $tmp_py_env/bin/activate             || exit 2
 
 #now inside the virtual env, python is ${PYTHON}
 
 # the following creates "build dist .eggs" in $CIPHER_DIR
 tmp_cur_dir=$(pwd)
-cd $CIPHER_DIR
-python -m pip install .
-cd $tmp_cur_dir
+cd $CIPHER_DIR          || exit 3
+python -m pip install . || exit 4
+cd $tmp_cur_dir         || exit 5
 unset tmp_cur_dir
 
 echo "Running python cipher test..."
@@ -37,8 +37,8 @@ do
     echo "Hello world $i" >> test.in
 done
 
-python ${CIPHER_DIR}/encrypt.py --password=password123 test.in test.enc
-python ${CIPHER_DIR}/decrypt.py --password=password123 test.enc test.out
+python ${CIPHER_DIR}/encrypt.py --password=password123 test.in test.enc ||exit 6
+python ${CIPHER_DIR}/decrypt.py --password=password123 test.enc test.out||exit 7
 
 diff test.in test.out
 exit_code=$?
@@ -47,16 +47,14 @@ rm test.in test.enc test.out
 echo "python cipher test is complete."
 
 # cleanup
-deactivate
+deactivate || exit 8
 #now outside the virtual env
 
 rm -rf $tmp_py_env
 unset tmp_py_env
 
-cd $CIPHER_DIR
-rm -r  build dist .eggs
+cd $CIPHER_DIR || exit 9
 rm -rf packit-0.18-py2.7.egg pbr-3.1.1-py2.7.egg wheel-0.30.0-py2.7.egg
-rm -v  cipher.egg-info/SOURCES.txt
 rm -vf glob2-0.4.1-py2.7.egg
 
 exit $exit_code
