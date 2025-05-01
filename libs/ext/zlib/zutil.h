e9089ca6481e75e6067b221aaa6fbf1bd6c2dc7b
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-01 11:27:13 -0700

    fix: conditional compilation for missing macos fdopen

--- libs/ext/zlib/zutil.h
+++ libs/ext/zlib/zutil.h
@@ -144,7 +144,9 @@ extern z_const char * const z_errmsg[10]; /* indexed by 2-zlib_error */
 #      include <unix.h> /* for fdopen */
 #    else
 #      ifndef fdopen
-#        define fdopen(fd,mode) NULL /* No fdopen() */
+#        if !defined(__MAC_15_4) || __MAC_OS_X_VERSION_MIN_REQUIRED > __MAC_15_4
+#          define fdopen(fd,mode) NULL /* No fdopen() */
+#        endif
 #      endif
 #    endif
 #  endif
