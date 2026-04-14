6c6eef47d180e3f85268a2203725ea49c4021c50
Andrew Klymenko <klymenka@ncbi.nlm.nih.gov>
2026-04-14 14:50:05 -0400

    VDB-6310: tests for validation of quality input string

--- test/vdb/test-quality.cpp
+++ test/vdb/test-quality.cpp
@@ -23,6 +23,7 @@
 * =========================================================================== */
 
 #include <kfg/kfg-priv.h> /* KConfigDisableUserSettings */
+#include <klib/text.h> // string_size
 #include <ktst/unit_test.hpp> // TEST_SUITE
 #include <vdb/vdb-priv.h> /* VDBManagerGetQualityString */
 #include "../libs/vdb/dbmgr-priv.h" /* VDBManagerQualityReset */
@@ -32,6 +33,32 @@ TEST_SUITE(TestQualSuite)
 #define ALL
 
 using std::string;
+
+#define PREFER_FULL string("RZ")
+#define PREFER_ZERO string("ZR")
+
+static bool IS_VALID(const char* s)
+{   return QualityInputValidForCgiCall(s, string_size(s)); }
+
+TEST_CASE(Validate){
+    REQUIRE(!QualityInputValidForCgiCall(NULL, 1));
+    REQUIRE(!QualityInputValidForCgiCall("12345678", 0));
+
+    REQUIRE(QualityInputValidForCgiCall("1Z!\"%&<=>", 1));
+
+    REQUIRE(!IS_VALID("!"));
+    REQUIRE(!IS_VALID("\""));
+    REQUIRE(!IS_VALID("%"));
+    REQUIRE(!IS_VALID("&"));
+    REQUIRE(!IS_VALID("<"));
+    REQUIRE(!IS_VALID("="));
+    REQUIRE(!IS_VALID(">"));
+
+    REQUIRE(!IS_VALID("123456789"));
+
+    REQUIRE(IS_VALID("12345678"));
+    REQUIRE(IS_VALID("AaRr_0Zz"));
+}
 
 TEST_CASE(Default) {
     KConfig* k(nullptr);
@@ -46,15 +73,45 @@ TEST_CASE(Default) {
 
     const char* quality(nullptr);
     REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("RZ"));
+    REQUIRE_EQ(string(quality), PREFER_FULL);
 
     REQUIRE_RC(VDBManagerPreferFullQuality(nullptr));
     REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("RZ"));
+    REQUIRE_EQ(string(quality), PREFER_FULL);
 
     REQUIRE_RC(VDBManagerPreferZeroQuality(nullptr));
     REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("ZR"));
+    REQUIRE_EQ(string(quality), PREFER_ZERO);
+
+    REQUIRE_RC(KConfigRelease(k));
+}
+
+TEST_CASE(ConfigLong) {
+    VDBManagerQualityReset();
+
+    KConfig* k(nullptr);
+    REQUIRE_RC(KConfigMake(&k, nullptr));
+
+    String* q(nullptr);
+    REQUIRE_RC(KConfigReadString(k, "OS", &q));
+    free(q); q = nullptr;
+
+    REQUIRE_RC_FAIL(KConfigReadString(k, "/libs/vdb/quality", &q));
+    REQUIRE_RC_FAIL(VDBManagerGetQualityString(nullptr, nullptr));
+
+    REQUIRE_RC(KConfigWriteString(k, "/libs/vdb/quality", "012345678ZR9_AaBb"));
+
+    const char* quality(nullptr);
+    REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
+    REQUIRE_EQ(string(quality), PREFER_FULL);
+
+    REQUIRE_RC(VDBManagerPreferZeroQuality(nullptr));
+    REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
+    REQUIRE_EQ(string(quality), PREFER_ZERO);
+
+    REQUIRE_RC(VDBManagerPreferFullQuality(nullptr));
+    REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
+    REQUIRE_EQ(string(quality), PREFER_FULL);
 
     REQUIRE_RC(KConfigRelease(k));
 }
@@ -72,19 +129,50 @@ TEST_CASE(Config) {
     REQUIRE_RC_FAIL(KConfigReadString(k, "/libs/vdb/quality", &q));
     REQUIRE_RC_FAIL(VDBManagerGetQualityString(nullptr, nullptr));
 
+    REQUIRE_RC(KConfigWriteString(k, "/libs/vdb/quality", "12345_ZR"));
+
+    const char* quality(nullptr);
+    REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
+    REQUIRE_EQ(string(quality), string("12345_ZR"));
+
+    REQUIRE_RC(VDBManagerPreferFullQuality(nullptr));
+    REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
+    REQUIRE_EQ(string(quality), PREFER_FULL);
+
+    REQUIRE_RC(VDBManagerPreferZeroQuality(nullptr));
+    REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
+    REQUIRE_EQ(string(quality), PREFER_ZERO);
+
+    REQUIRE_RC(KConfigRelease(k));
+}
+
+TEST_CASE(EnvLong) {
+    VDBManagerQualityReset();
+
+    KConfig* k(nullptr);
+    REQUIRE_RC(KConfigMake(&k, nullptr));
+
+    String* q(nullptr);
+    REQUIRE_RC(KConfigReadString(k, "OS", &q));
+    free(q); q = nullptr;
+
+    REQUIRE_RC_FAIL(KConfigReadString(k, "/libs/vdb/quality", &q));
+    REQUIRE_RC_FAIL(VDBManagerGetQualityString(nullptr, nullptr));
+
+    REQUIRE_EQ(putenv((char*)"NCBI_VDB_QUALITY=bBaA_9RZ976543210"), 0);
     REQUIRE_RC(KConfigWriteString(k, "/libs/vdb/quality", "012345678ZR9_AaBb"));
 
     const char* quality(nullptr);
     REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("012345678ZR9_AaBb"));
-
-    REQUIRE_RC(VDBManagerPreferFullQuality(nullptr));
-    REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("RZ"));
+    REQUIRE_EQ(string(quality), PREFER_FULL);
 
     REQUIRE_RC(VDBManagerPreferZeroQuality(nullptr));
     REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("ZR"));
+    REQUIRE_EQ(string(quality), PREFER_ZERO);
+
+    REQUIRE_RC(VDBManagerPreferFullQuality(nullptr));
+    REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
+    REQUIRE_EQ(string(quality), PREFER_FULL);
 
     REQUIRE_RC(KConfigRelease(k));
 }
@@ -103,19 +191,19 @@ TEST_CASE(Env) {
     REQUIRE_RC_FAIL(VDBManagerGetQualityString(nullptr, nullptr));
 
     REQUIRE_EQ(putenv((char*)"NCBI_VDB_QUALITY=bBaA_9RZ976543210"), 0);
-    REQUIRE_RC(KConfigWriteString(k, "/libs/vdb/quality", "012345678ZR9_AaBb"));
+    REQUIRE_RC(KConfigWriteString(k, "/libs/vdb/quality", "ZR9_AaBb"));
 
     const char* quality(nullptr);
     REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("bBaA_9RZ976543210"));
+    REQUIRE_EQ(string(quality), string("ZR9_AaBb"));
 
     REQUIRE_RC(VDBManagerPreferFullQuality(nullptr));
     REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("RZ"));
+    REQUIRE_EQ(string(quality), PREFER_FULL);
 
     REQUIRE_RC(VDBManagerPreferZeroQuality(nullptr));
     REQUIRE_RC(VDBManagerGetQualityString(nullptr, &quality));
-    REQUIRE_EQ(string(quality), string("ZR"));
+    REQUIRE_EQ(string(quality), PREFER_ZERO);
 
     REQUIRE_RC(KConfigRelease(k));
 }
