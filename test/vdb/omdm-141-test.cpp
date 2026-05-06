4425ad36e8cb1beaf6c0ca52518f30a77aa53b87
aboshkin <boshkina@ncbi.nlm.nih.gov>
2026-05-06 09:28:35 -0400

    debugging MT test

--- test/vdb/omdm-141-test.cpp
+++ test/vdb/omdm-141-test.cpp
@@ -73,16 +73,17 @@ void SDKUnlock() {
     if (use_mutex) sdk_mutex.unlock();
 }
 
-
-void CheckRc(rc_t rc, const char* code, const char* file, int line)
+#define REPORT_0_RC 0
+void CheckRc(int idx, rc_t rc, const char* code, const char* file, int line)
 {
-    if (rc) {
+    if (rc)
+    {
         char buffer1[4096];
         size_t error_len;
         RCExplain(rc, buffer1, sizeof(buffer1), &error_len);
         char buffer2[8192];
-        int len = snprintf(buffer2, sizeof(buffer2), "%s:%d: %s failed: %#x: %s",
-            file, line, code, rc, buffer1);
+        int len = snprintf(buffer2, sizeof(buffer2), "Thread #%i %s:%d: %s failed: %#x: %s",
+            idx, file, line, code, rc, buffer1);
         if (int(sizeof(buffer2)) < len) len = int(sizeof(buffer2));
         string msg(buffer2, len);
         SDKLock();
@@ -90,15 +91,28 @@ void CheckRc(rc_t rc, const char* code, const char* file, int line)
         SDKUnlock();
         exit(1);
     }
+    else
+    {
+#if REPORT_0_RC
+        char buffer2[8192];
+        int len = snprintf(buffer2, sizeof(buffer2), "Thread #%i %s:%d: %s OK",
+            idx, file, line, code);
+        if (int(sizeof(buffer2)) < len) len = int(sizeof(buffer2));
+        string msg(buffer2, len);
+        SDKLock();
+        cout << "RC: " << msg << endl;
+        SDKUnlock();
+#endif
+    }
 }
 
 
-#define RC_CALL(call) \
+#define RC_CALL(idx,call) \
     do { \
         SDKLock(); \
         rc_t rc = (call); \
         SDKUnlock(); \
-        CheckRc(rc, #call, __FILE__, __LINE__); \
+        CheckRc(idx,rc, #call, __FILE__, __LINE__); \
     } while (0)
 
 int LowLevelTest(int idx, const string& acc)
@@ -107,37 +121,37 @@ int LowLevelTest(int idx, const string& acc)
     cout << "thread " << idx << " id " << acc << endl;
     OUTUnlock();
     const VDBManager* mgr = 0;
-    RC_CALL(VDBManagerMakeRead(&mgr, 0));
+    RC_CALL(idx,VDBManagerMakeRead(&mgr, 0));
 
     const VDatabase* db = 0;
-    RC_CALL(VDBManagerOpenDBRead(mgr, &db, 0, acc.c_str()));
+    RC_CALL(idx,VDBManagerOpenDBRead(mgr, &db, 0, acc.c_str()));
 
     const VTable* seq_table = 0;
-    RC_CALL(VDatabaseOpenTableRead(db, &seq_table, "SEQUENCE"));
+    RC_CALL(idx,VDatabaseOpenTableRead(db, &seq_table, "SEQUENCE"));
 
     const VCursor* seq_cursor = 0;
-    RC_CALL(VTableCreateCursorRead(seq_table, &seq_cursor));
-    RC_CALL(VCursorPermitPostOpenAdd(seq_cursor));
-    RC_CALL(VCursorOpen(seq_cursor));
+    RC_CALL(idx,VTableCreateCursorRead(seq_table, &seq_cursor));
+    RC_CALL(idx,VCursorPermitPostOpenAdd(seq_cursor));
+    RC_CALL(idx,VCursorOpen(seq_cursor));
 
     uint32_t SPOT_GROUP;
-    RC_CALL(VCursorAddColumn(seq_cursor, &SPOT_GROUP, "SPOT_GROUP"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &SPOT_GROUP, "SPOT_GROUP"));
     uint32_t READ_TYPE;
-    RC_CALL(VCursorAddColumn(seq_cursor, &READ_TYPE, "READ_TYPE"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &READ_TYPE, "READ_TYPE"));
     uint32_t READ_LEN;
-    RC_CALL(VCursorAddColumn(seq_cursor, &READ_LEN, "READ_LEN"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &READ_LEN, "READ_LEN"));
     uint32_t READ_START;
-    RC_CALL(VCursorAddColumn(seq_cursor, &READ_START, "READ_START"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &READ_START, "READ_START"));
     uint32_t READ;
-    RC_CALL(VCursorAddColumn(seq_cursor, &READ, "READ"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &READ, "READ"));
     uint32_t QUALITY;
-    RC_CALL(VCursorAddColumn(seq_cursor, &QUALITY, "QUALITY"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &QUALITY, "QUALITY"));
     uint32_t TRIM_LEN;
-    RC_CALL(VCursorAddColumn(seq_cursor, &TRIM_LEN, "TRIM_LEN"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &TRIM_LEN, "TRIM_LEN"));
     uint32_t TRIM_START;
-    RC_CALL(VCursorAddColumn(seq_cursor, &TRIM_START, "TRIM_START"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &TRIM_START, "TRIM_START"));
     uint32_t NAME;
-    RC_CALL(VCursorAddColumn(seq_cursor, &NAME, "NAME"));
+    RC_CALL(idx,VCursorAddColumn(seq_cursor, &NAME, "NAME"));
 
     int64_t seq_row = 38012;
     {
@@ -145,140 +159,140 @@ int LowLevelTest(int idx, const string& acc)
         uint32_t bit_offset, bit_length;
         uint32_t elem_count;
         for (int i = 0; i < 2; ++i) {
-            RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+            RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
                 READ_TYPE,
                 &bit_length, &data, &bit_offset,
                 &elem_count));
-            RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+            RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
                 READ_TYPE,
                 &bit_length, &data, &bit_offset,
                 &elem_count));
-            RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+            RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
                 READ_LEN,
                 &bit_length, &data, &bit_offset,
                 &elem_count));
-            RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+            RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
                 READ_START,
                 &bit_length, &data, &bit_offset,
                 &elem_count));
-            RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+            RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
                 TRIM_START,
                 &bit_length, &data, &bit_offset,
                 &elem_count));
-            RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+            RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
                 TRIM_LEN,
                 &bit_length, &data, &bit_offset,
                 &elem_count));
         }
 
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             NAME,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             NAME,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ_LEN,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ_START,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             TRIM_START,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             TRIM_LEN,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ,
             &bit_length, &data, &bit_offset,
             &elem_count));
 
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ_TYPE,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ_LEN,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ_START,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             TRIM_START,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             TRIM_LEN,
             &bit_length, &data, &bit_offset,
             &elem_count));
 
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             NAME,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             NAME,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ_LEN,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ_START,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             TRIM_START,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             TRIM_LEN,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row,
             READ,
             &bit_length, &data, &bit_offset,
             &elem_count));
 
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row + 1,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row + 1,
             READ_TYPE,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row + 1,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row + 1,
             READ_TYPE,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row + 1,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row + 1,
             READ_LEN,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row + 1,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row + 1,
             READ_START,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row + 1,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row + 1,
             READ_START,
             &bit_length, &data, &bit_offset,
             &elem_count));
-        RC_CALL(VCursorCellDataDirect(seq_cursor, seq_row + 1,
+        RC_CALL(idx,VCursorCellDataDirect(seq_cursor, seq_row + 1,
             TRIM_LEN,
             &bit_length, &data, &bit_offset,
             &elem_count));
     }
-    RC_CALL(VCursorRelease(seq_cursor));
-    RC_CALL(VTableRelease(seq_table));
-    RC_CALL(VDatabaseRelease(db));
-    RC_CALL(VDBManagerRelease(mgr));
+    RC_CALL(idx,VCursorRelease(seq_cursor));
+    RC_CALL(idx,VTableRelease(seq_table));
+    RC_CALL(idx,VDatabaseRelease(db));
+    RC_CALL(idx,VDBManagerRelease(mgr));
     return 0;
 }
 
